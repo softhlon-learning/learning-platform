31ef39a630784fad02570f891ca25a34b6d82065
softhlon-learning <softhlonlearning@gmail.com>
2025-01-04 17:00:27 +0100

    Draft implementation of subscription services.

---
+++ backend/subscriptions/src/main/java/io/softhlon/learning/subscriptions/domain/SubscribeServiceImpl.java
@@ -0,0 +1,17 @@
+// ---------------------------------------------------------------------------------------------------------------------
+// Copyright (C) IO.SOFTHLON - All Rights Reserved
+// Unauthorized copying of this file via any medium is strongly encouraged.
+// ---------------------------------------------------------------------------------------------------------------------
+
+package io.softhlon.learning.subscriptions.domain;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Implementation
+// ---------------------------------------------------------------------------------------------------------------------
+
+class SubscribeServiceImpl implements SubscribeService {
+    @Override
+    public Response subscribe(Request request) {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ backend/subscriptions/src/main/java/io/softhlon/learning/subscriptions/domain/UnsubscribeServiceImpl.java
@@ -0,0 +1,17 @@
+// ---------------------------------------------------------------------------------------------------------------------
+// Copyright (C) IO.SOFTHLON - All Rights Reserved
+// Unauthorized copying of this file via any medium is strongly encouraged.
+// ---------------------------------------------------------------------------------------------------------------------
+
+package io.softhlon.learning.subscriptions.domain;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Implementation
+// ---------------------------------------------------------------------------------------------------------------------
+
+class UnsubscribeServiceImpl implements UnsubscribeService {
+    @Override
+    public Response unsubscribe(Request request) {
+        throw new UnsupportedOperationException();
+    }
+}
