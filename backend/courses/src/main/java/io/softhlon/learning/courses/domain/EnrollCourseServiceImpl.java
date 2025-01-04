193367b2dfcdbac606aa7f2d2b3a7fab1c64d0da
softhlon-learning <softhlonlearning@gmail.com>
2025-01-04 16:52:04 +0100

    Draft implementation of course services.

---
+++ backend/courses/src/main/java/io/softhlon/learning/courses/domain/EnrollCourseServiceImpl.java
@@ -0,0 +1,17 @@
+// ---------------------------------------------------------------------------------------------------------------------
+// Copyright (C) IO.SOFTHLON - All Rights Reserved
+// Unauthorized copying of this file via any medium is strongly encouraged.
+// ---------------------------------------------------------------------------------------------------------------------
+
+package io.softhlon.learning.courses.domain;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Implementation
+// ---------------------------------------------------------------------------------------------------------------------
+
+class EnrollCourseServiceImpl implements EnrollCourseService {
+    @Override
+    public Response enroll(Request request) {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ backend/courses/src/main/java/io/softhlon/learning/courses/domain/GetCourseDetailsServiceImpl.java
@@ -0,0 +1,17 @@
+// ---------------------------------------------------------------------------------------------------------------------
+// Copyright (C) IO.SOFTHLON - All Rights Reserved
+// Unauthorized copying of this file via any medium is strongly encouraged.
+// ---------------------------------------------------------------------------------------------------------------------
+
+package io.softhlon.learning.courses.domain;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Implementation
+// ---------------------------------------------------------------------------------------------------------------------
+
+class GetCourseDetailsServiceImpl implements GetCourseDetailsService {
+    @Override
+    public Response getDetails(Request request) {
+        throw new UnsupportedOperationException();
+    }
+}
