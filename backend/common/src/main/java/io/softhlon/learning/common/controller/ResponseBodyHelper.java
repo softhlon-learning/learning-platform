670bf77d50b40455f458d8750a22852bf36191f5
softhlon-learning <softhlonlearning@gmail.com>
2025-01-04 22:13:38 +0100

    Added ResponseBodyHelper.

---
+++ backend/common/src/main/java/io/softhlon/learning/common/controller/ResponseBodyHelper.java
@@ -0,0 +1,93 @@
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------
+// Copyright (C) IO.PMARAT - All Rights Reserved
+// Unauthorized copying of this file via any medium is strongly encouraged.
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------
+package io.softhlon.learning.common.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.Builder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.time.OffsetDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import static io.softhlon.learning.common.controller.ResponseBodyHelper.ResponseField.*;
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------
+// Implementation
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+@Slf4j
+public class ResponseBodyHelper {
+  public static ResponseEntity<?> internalServerBody(HttpServletRequest request, Throwable cause) {
+    log.error("Unexpected error:", cause);
+    return ResponseEntity.internalServerError()
+        .body(
+            ResponseBodyBuilder.builder()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .errorMessage(ErrorMessage.INTERNAL_SERVER_ERROR)
+                .message(cause.getMessage())
+                .path(request.getRequestURI())
+                .build()
+                .body());
+  }
+
+  public static ResponseEntity<?> badRequestBody(HttpServletRequest request, String message) {
+    return ResponseEntity.badRequest()
+        .body(
+            ResponseBodyBuilder.builder()
+                .status(HttpStatus.BAD_REQUEST)
+                .errorMessage(ErrorMessage.VALIDATION_ERROR)
+                .message(message)
+                .path(request.getRequestURI())
+                .build()
+                .body());
+  }
+
+  @Builder
+  static class ResponseBodyBuilder {
+    private HttpStatus status;
+    private ErrorMessage errorMessage;
+    private Object message;
+    private String path;
+
+    Map<String, Object> body() {
+      Map<String, Object> responseBody = new LinkedHashMap<>();
+      responseBody.put(TIMESTAMP.text, OffsetDateTime.now());
+      responseBody.put(STATUS.text, status.value());
+      responseBody.put(ERROR.text, errorMessage.text);
+      responseBody.put(MESSAGE.text, message);
+      responseBody.put(PATH.text, path);
+
+      return responseBody;
+    }
+  }
+
+  enum ResponseField {
+    TIMESTAMP("timestamp"),
+    STATUS("status"),
+    ERROR("error"),
+    MESSAGE("message"),
+    PATH("path");
+
+    private final String text;
+
+    ResponseField(String text) {
+      this.text = text;
+    }
+  }
+
+  enum ErrorMessage {
+    VALIDATION_ERROR("Validation Error"),
+    INTERNAL_SERVER_ERROR("Internal Server Error");
+
+    private final String text;
+
+    ErrorMessage(String text) {
+      this.text = text;
+    }
+  }
+}
--- backend/common/src/main/java/module-info.java
+++ backend/common/src/main/java/module-info.java
@@ -1,4 +1,10 @@
 module common {
+    requires static lombok;
+    requires spring.context;
+    requires spring.web;
+    requires org.apache.tomcat.embed.core;
+    requires org.slf4j;
+
     exports io.softhlon.learning.common.domain;
     exports io.softhlon.learning.common.event;
     exports io.softhlon.learning.common.hexagonal;
