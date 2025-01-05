553c9b482c675cdf4a9352f413ea3a592de11290
softhlon-learning <softhlonlearning@gmail.com>
2025-01-06 00:07:26 +0100

    Created module-info.java files for all modules.

---
+++ backend/subscriptions/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module subscriptions {
+    requires common;
+    requires static lombok;
+    requires spring.context;
+    requires spring.web;
+    requires org.apache.tomcat.embed.core;
+    requires org.slf4j;
+}
