9786eaae077fa5bcb46f43fe377387edc863102b
softhlon-learning <softhlonlearning@gmail.com>
2025-01-05 23:45:21 +0100

    Reformatting.

--- backend/common/src/main/java/io/softhlon/learning/common/controller/ResponseBodyHelper.java
+++ backend/common/src/main/java/io/softhlon/learning/common/controller/ResponseBodyHelper.java
@@ -22,72 +22,72 @@ import static io.softhlon.learning.common.controller.ResponseBodyHelper.Response
 
 @Slf4j
 public class ResponseBodyHelper {
-  public static ResponseEntity<?> internalServerBody(HttpServletRequest request, Throwable cause) {
-    log.error("Unexpected error:", cause);
-    return ResponseEntity.internalServerError()
-        .body(
-            ResponseBodyBuilder.builder()
-                .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .errorMessage(ErrorMessage.INTERNAL_SERVER_ERROR)
-                .message(cause.getMessage())
-                .path(request.getRequestURI())
-                .build()
-                .body());
-  }
-
-  public static ResponseEntity<?> badRequestBody(HttpServletRequest request, String message) {
-    return ResponseEntity.badRequest()
-        .body(
-            ResponseBodyBuilder.builder()
-                .status(HttpStatus.BAD_REQUEST)
-                .errorMessage(ErrorMessage.VALIDATION_ERROR)
-                .message(message)
-                .path(request.getRequestURI())
-                .build()
-                .body());
-  }
-
-  @Builder
-  static class ResponseBodyBuilder {
-    private HttpStatus status;
-    private ErrorMessage errorMessage;
-    private Object message;
-    private String path;
-
-    Map<String, Object> body() {
-      Map<String, Object> responseBody = new LinkedHashMap<>();
-      responseBody.put(TIMESTAMP.text, OffsetDateTime.now());
-      responseBody.put(STATUS.text, status.value());
-      responseBody.put(ERROR.text, errorMessage.text);
-      responseBody.put(MESSAGE.text, message);
-      responseBody.put(PATH.text, path);
-
-      return responseBody;
+    public static ResponseEntity<?> internalServerBody(HttpServletRequest request, Throwable cause) {
+        log.error("Unexpected error:", cause);
+        return ResponseEntity.internalServerError()
+              .body(
+                    ResponseBodyBuilder.builder()
+                          .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                          .errorMessage(ErrorMessage.INTERNAL_SERVER_ERROR)
+                          .message(cause.getMessage())
+                          .path(request.getRequestURI())
+                          .build()
+                          .body());
     }
-  }
 
-  enum ResponseField {
-    TIMESTAMP("timestamp"),
-    STATUS("status"),
-    ERROR("error"),
-    MESSAGE("message"),
-    PATH("path");
-
-    private final String text;
-
-    ResponseField(String text) {
-      this.text = text;
+    public static ResponseEntity<?> badRequestBody(HttpServletRequest request, String message) {
+        return ResponseEntity.badRequest()
+              .body(
+                    ResponseBodyBuilder.builder()
+                          .status(HttpStatus.BAD_REQUEST)
+                          .errorMessage(ErrorMessage.VALIDATION_ERROR)
+                          .message(message)
+                          .path(request.getRequestURI())
+                          .build()
+                          .body());
     }
-  }
 
-  enum ErrorMessage {
-    VALIDATION_ERROR("Validation Error"),
-    INTERNAL_SERVER_ERROR("Internal Server Error");
+    @Builder
+    static class ResponseBodyBuilder {
+        private HttpStatus status;
+        private ErrorMessage errorMessage;
+        private Object message;
+        private String path;
 
-    private final String text;
+        Map<String, Object> body() {
+            Map<String, Object> responseBody = new LinkedHashMap<>();
+            responseBody.put(TIMESTAMP.text, OffsetDateTime.now());
+            responseBody.put(STATUS.text, status.value());
+            responseBody.put(ERROR.text, errorMessage.text);
+            responseBody.put(MESSAGE.text, message);
+            responseBody.put(PATH.text, path);
 
-    ErrorMessage(String text) {
-      this.text = text;
+            return responseBody;
+        }
     }
-  }
+
+    enum ResponseField {
+        TIMESTAMP("timestamp"),
+        STATUS("status"),
+        ERROR("error"),
+        MESSAGE("message"),
+        PATH("path");
+
+        private final String text;
+
+        ResponseField(String text) {
+            this.text = text;
+        }
+    }
+
+    enum ErrorMessage {
+        VALIDATION_ERROR("Validation Error"),
+        INTERNAL_SERVER_ERROR("Internal Server Error");
+
+        private final String text;
+
+        ErrorMessage(String text) {
+            this.text = text;
+        }
+    }
 }
