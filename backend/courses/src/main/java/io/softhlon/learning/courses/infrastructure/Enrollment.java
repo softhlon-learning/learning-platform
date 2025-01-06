18c92bcef5957f9062052164df648fd65abc5930
softhlon-learning <softhlonlearning@gmail.com>
2025-01-06 16:53:40 +0100

    Jpa repository and entity for courses (enrollments).

---
+++ backend/courses/src/main/java/io/softhlon/learning/courses/infrastructure/Enrollment.java
@@ -0,0 +1,36 @@
+// ---------------------------------------------------------------------------------------------------------------------
+// Copyright (C) IO.SOFTHLON - All Rights Reserved
+// Unauthorized copying of this file via any medium is strongly encouraged.
+// ---------------------------------------------------------------------------------------------------------------------
+
+package io.softhlon.learning.courses.infrastructure;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Implementation
+// ---------------------------------------------------------------------------------------------------------------------
+
+@Getter
+@Builder
+@Entity(name = "enrollments")
+public class Enrollment {
+    @Id
+    private UUID id;
+    @ManyToOne
+    private Course course;
+    private String status;
+    private String content;
+    private OffsetDateTime enrolledTime;
+    private OffsetDateTime unenrolledTime;
+    private OffsetDateTime completedTime;
+    private OffsetDateTime createdTime;
+    private OffsetDateTime updatedTime;
+}
---
+++ backend/courses/src/main/java/io/softhlon/learning/courses/infrastructure/EnrollmentsJpaRepository.java
@@ -0,0 +1,19 @@
+// ---------------------------------------------------------------------------------------------------------------------
+// Copyright (C) IO.SOFTHLON - All Rights Reserved
+// Unauthorized copying of this file via any medium is strongly encouraged.
+// ---------------------------------------------------------------------------------------------------------------------
+
+package io.softhlon.learning.courses.infrastructure;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Implementation
+// ---------------------------------------------------------------------------------------------------------------------
+
+@Repository
+interface EnrollmentsJpaRepository extends CrudRepository<Enrollment, UUID> {
+}
