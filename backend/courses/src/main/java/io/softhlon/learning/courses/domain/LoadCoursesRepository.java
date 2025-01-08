8a68ce246674cd65be01b7263208c64e49e726bb
softhlon-learning <softhlonlearning@gmail.com>
2025-01-08 17:59:57 +0100

    Added LoadCoursesRepository and implemented its adapter.

---
+++ backend/courses/src/main/java/io/softhlon/learning/courses/domain/LoadCoursesRepository.java
@@ -0,0 +1,34 @@
+// ---------------------------------------------------------------------------------------------------------------------
+// Copyright (C) IO.SOFTHLON - All Rights Reserved
+// Unauthorized copying of this file via any medium is strongly encouraged.
+// ---------------------------------------------------------------------------------------------------------------------
+
+package io.softhlon.learning.courses.domain;
+
+import io.softhlon.learning.common.domain.DomainRepository;
+import io.softhlon.learning.common.hexagonal.OutboundPort;
+
+import java.util.List;
+import java.util.UUID;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Implementation
+// ---------------------------------------------------------------------------------------------------------------------
+
+@OutboundPort
+@DomainRepository
+@FunctionalInterface
+public interface LoadCoursesRepository {
+    LoadCoursesResult execute();
+
+    record Course(
+          UUID courseId,
+          String name,
+          String description,
+          String version) {}
+
+    sealed interface LoadCoursesResult {
+        record CoursesLoaded(List<Course> course) implements LoadCoursesResult {}
+        record CoursesLoadFailed(Throwable cause) implements LoadCoursesResult {}
+    }
+}
---
+++ backend/courses/src/main/java/io/softhlon/learning/courses/infrastructure/LoadCoursesRepositoryAdapter.java
@@ -0,0 +1,55 @@
+// ---------------------------------------------------------------------------------------------------------------------
+// Copyright (C) IO.SOFTHLON - All Rights Reserved
+// Unauthorized copying of this file via any medium is strongly encouraged.
+// ---------------------------------------------------------------------------------------------------------------------
+
+package io.softhlon.learning.courses.infrastructure;
+
+import io.softhlon.learning.common.hexagonal.PersistenceAdapter;
+import io.softhlon.learning.courses.domain.LoadCoursesRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+import java.util.stream.StreamSupport;
+
+import static io.softhlon.learning.courses.domain.LoadCoursesRepository.LoadCoursesResult.CoursesLoadFailed;
+import static io.softhlon.learning.courses.domain.LoadCoursesRepository.LoadCoursesResult.CoursesLoaded;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Implementation
+// ---------------------------------------------------------------------------------------------------------------------
+
+@Slf4j
+@Service
+@PersistenceAdapter
+@RequiredArgsConstructor
+class LoadCoursesRepositoryAdapter implements LoadCoursesRepository {
+    private final CoursesJpaRepository coursesRepo;
+
+    @Override
+    public LoadCoursesResult execute() {
+        try {
+            var stream = StreamSupport.stream(coursesRepo.findAll().spliterator(), false);
+            var courses = stream.map(this::toCourse).toList();
+            return new CoursesLoaded(courses);
+        } catch (Throwable cause) {
+            log.error("Error", cause);
+            return new CoursesLoadFailed(cause);
+        }
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+    // Private Section
+    // -----------------------------------------------------------------------------------------------------------------
+
+    private Course toCourse(CourseEntity entity) {
+        return new Course(
+              entity.getId(),
+              entity.getName(),
+              entity.getDescription(),
+              entity.getVersion()
+        );
+    }
+}
