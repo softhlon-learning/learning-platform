36c4a41b0233a5eae192a82ec63867f9f88956ce
softhlon-learning <softhlonlearning@gmail.com>
2025-01-13 12:18:57 +0100

    Created google-signin component.

---
+++ frontend/src/app/google-signin/google-signin.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-google-signin',
+  standalone: true,
+  imports: [],
+  templateUrl: './google-signin.component.html',
+  styleUrl: './google-signin.component.css'
+})
+export class GoogleSigninComponent {
+
+}
