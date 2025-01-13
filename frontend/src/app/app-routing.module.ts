6f80628ab8196f5e9c2fe2e3aaccb647015aa7db
softhlon-learning <softhlonlearning@gmail.com>
2025-01-13 14:36:51 +0100

    Renamed sign-up to sign-in and moved continue with google button to it.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,11 +4,11 @@ import {RouterModule, Routes} from '@angular/router';
 import {CoursesComponent} from './courses/courses.component';
 import {CourseTocComponent} from './course-toc/course-toc.component';
 import {CourseProgressComponent} from "./course-progress/course-progress.component";
-import {SignUpComponent} from "./sign-up/sign-up.component";
+import {SignInComponent} from "./sign-in/sign-in.component";
 
 const routes: Routes = [
     {path: '', redirectTo: '/courses', pathMatch: 'full'},
-    {path: 'sign-up', component: SignUpComponent},
+    {path: 'sign-in', component: SignInComponent},
     {path: 'courses', component: CoursesComponent},
     {path: 'course/:id', component: CourseTocComponent},
     {path: 'course/:id/progress', component: CourseProgressComponent}
