cc13060931a30cf4b0f8c2240353510e1e5ab9eb
softhlon-learning <softhlonlearning@gmail.com>
2025-01-14 14:59:56 +0100

    Added CreateInvalidatedTokenRepository and its empty adapter.

---
+++ backend/accounts/src/main/java/io/softhlon/learning/accounts/domain/CreateInvalidatedTokenRepository.java
@@ -0,0 +1,28 @@
+// ---------------------------------------------------------------------------------------------------------------------
+// Copyright (C) SOFTHLON-LEARNING.TECH - All Rights Reserved
+// Unauthorized copying of this file via any medium is strongly encouraged.
+// ---------------------------------------------------------------------------------------------------------------------
+
+package io.softhlon.learning.accounts.domain;
+
+import tech.softhlon.learning.common.domain.DomainRepository;
+import tech.softhlon.learning.common.hexagonal.OutboundPort;
+
+import java.util.UUID;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Implementation
+// ---------------------------------------------------------------------------------------------------------------------
+
+@OutboundPort
+@DomainRepository
+@FunctionalInterface
+public interface CreateInvalidatedTokenRepository {
+    CreateInvalidatedTokenResult execute(CreateInvalidatedTokenRequest request);
+
+    sealed interface CreateInvalidatedTokenResult {
+        record InvalidatedTokenPersisted(UUID uuid) implements CreateInvalidatedTokenResult {}
+        record InvalidatedTokenPersistenceFailed(Throwable cause) implements CreateInvalidatedTokenResult {}
+    }
+    record CreateInvalidatedTokenRequest(String tokenHash) {}
+}
