0701a6755c4a00e9c283fd651648e5a50bc5a23c
softhlon-learning <softhlonlearning@gmail.com>
2025-01-14 13:16:19 +0100

    Partial implementation of GoogleSignInServiceImpl.

--- backend/accounts/src/main/java/io/softhlon/learning/accounts/domain/GoogleSignInServiceImpl.java
+++ backend/accounts/src/main/java/io/softhlon/learning/accounts/domain/GoogleSignInServiceImpl.java
@@ -9,47 +9,56 @@ import com.google.api.client.auth.openidconnect.IdToken;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.gson.GsonFactory;
+import io.softhlon.learning.accounts.domain.GoogleSignInService.Result.Failed;
+import io.softhlon.learning.accounts.domain.GoogleSignInService.Result.InvalidCredentialsFailed;
 import io.softhlon.learning.accounts.domain.GoogleSignInService.Result.Succeeded;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
-import java.security.GeneralSecurityException;
 import java.util.Collections;
 
-// ---------------------------------------------------------------------------------------------------------------------
+import static io.softhlon.learning.accounts.domain.CheckAccountByEmailRepository.CheckAccountByEmailResult.*;
+
+// --------------------------------------------------------------------------------------------------------------------
 // Implementation
 // ---------------------------------------------------------------------------------------------------------------------
 
 @Service
 class GoogleSignInServiceImpl implements GoogleSignInService {
     private final GoogleIdTokenVerifier verifier;
+    private final CheckAccountByEmailRepository checkAccountByEmailRepository;
 
-    public GoogleSignInServiceImpl(@Value("${google-client-id}") String clientId) {
+    public GoogleSignInServiceImpl(
+          @Value("${google-client-id}")
+          String clientId, CheckAccountByEmailRepository checkAccountByEmailRepository) {
         var transport = new NetHttpTransport();
         var jsonFactory = GsonFactory.getDefaultInstance();
         verifier = new GoogleIdTokenVerifier.Builder(transport, jsonFactory)
               .setAudience(Collections.singletonList(clientId))
               .build();
+        this.checkAccountByEmailRepository = checkAccountByEmailRepository;
     }
 
     @Override
     public Result signIn(Request request) {
         try {
             var idToken = verifier.verify(request.credential());
-
             if (idToken != null) {
                 IdToken.Payload payload = idToken.getPayload();
                 var email = (String) payload.get("email");
                 var name = (String) payload.get("given_name");
-            } else {
-            }
-        } catch (GeneralSecurityException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
 
-        return new Succeeded();
+                var exists = checkAccountByEmailRepository.execute(new CheckAccountByEmailRepository.CheckAccountByEmailRequest(email));
+                return switch (exists) {
+                    case AccountExists() -> new Succeeded();
+                    case AccountNotFound() -> null;
+                    case CheckAccountFailed(Throwable cause) -> new Result.Failed(cause);
+                };
+            } else {
+                return new InvalidCredentialsFailed("Invalid token/credentials");
+            }
+        } catch (Throwable cause) {
+            return new Failed(cause);
+        }
     }
 }
