d05b91810a762a78a55c10e63f1868b894606d8b
softhlon-learning <softhlonlearning@gmail.com>
2025-01-14 03:06:10 +0100

    Prepared run-local.sh and run-prod.sh scripts.

---
+++ backend/application/run-prod.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+cd ..
+mvn clean install
+if [ $? -eq 0 ]; then
+else
+  echo "Build failed. Application will not be started"
+  cd application
+  exit 1
+fi
+cd application
+mvn spring-boot:run -Dspring-boot.run.profiles=production
