04a8041f3e228c82b1059dc0f434eff386442fe8
softhlon-learning <softhlonlearning@gmail.com>
2025-01-15 23:39:35 +0100

    Secured backend with JWT.

--- backend/accounts/src/main/java/tech/softhlon/learning/accounts/domain/JwtAuthenticationFilter.java
+++ backend/accounts/src/main/java/tech/softhlon/learning/accounts/domain/JwtAuthenticationFilter.java
@@ -7,8 +7,13 @@ package tech.softhlon.learning.accounts.domain;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -19,11 +24,38 @@ import java.io.IOException;
 // ---------------------------------------------------------------------------------------------------------------------
 
 @Component
+@RequiredArgsConstructor
 class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
+
     @Override
     protected void doFilterInternal(
           HttpServletRequest request,
           HttpServletResponse response,
           FilterChain filterChain) throws ServletException, IOException {
+
+        var token = extractToken(request);
+        if (token != null && jwtService.isTokenValid(token)) {
+            var authentication = SecurityContextHolder.getContext().getAuthentication();
+            var user = new User();
+            var claims = jwtService.getAllClaimsFromToken(token);
+            user.accountId((String) claims.get("accountId"));
+            var authToken = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+
+            authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            SecurityContextHolder.getContext().setAuthentication(authToken);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String extractToken(HttpServletRequest request) {
+        var cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("Authorization")) {
+                return cookie.getValue();
+            }
+        }
+        return null;
     }
 }
