bc1325e3a61dd76544123db5b2bc4bccddd7a7a2
softhlon-learning <softhlonlearning@gmail.com>
2025-01-16 03:04:54 +0100

    Reformatting and optimized imports.

--- backend/accounts/src/main/java/tech/softhlon/learning/accounts/domain/JwtAuthenticationFilter.java
+++ backend/accounts/src/main/java/tech/softhlon/learning/accounts/domain/JwtAuthenticationFilter.java
@@ -11,7 +11,6 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
