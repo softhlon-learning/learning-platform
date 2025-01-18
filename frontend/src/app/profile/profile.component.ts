84569bc196608c964488605e522eb76120c5780d
softhlon-learning <softhlonlearning@gmail.com>
2025-01-19 00:38:05 +0100

    Integrated delete button with deleteAccount service.

--- frontend/src/app/profile/profile.component.ts
+++ frontend/src/app/profile/profile.component.ts
@@ -13,49 +13,32 @@ export class ProfileComponent implements OnInit {
     protected readonly environment = environment;
     error: string | undefined;
 
-    signInForm = this.formBuilder.group({
-        name: '',
-        email: '',
-        password: ''
-    });
 
     constructor(
-        private formBuilder: FormBuilder,
         private platformService: PlatformService,
         private router: Router) {
     }
 
     ngOnInit() {
         this.error = undefined;
-        const button = document.querySelector("#g_id_onload") as HTMLElement | null;
-        button?.setAttribute("data-client_id", environment.googleClientId);
-        button?.setAttribute("data-login_uri", environment.loginUri);
     }
 
-    onSubmit(): void {
-        if (this.signInForm.invalid) {
-            console.log('Form is invalid');
-            this.error = 'Please provide valid email and password';
-            return;
-        }
-
-        const {name = '', email = '', password = ''} = this.signInForm.value;
+    deleteAccount(): void {
         const DEFAULT_ERROR_MESSAGE = 'An unexpected error occurred';
-
-        this.platformService.signUp(name || '', email || '', password || '').subscribe({
-            next: () => this.handleSignInSuccess(),
-            error: (signInError) => this.handleSignInError(signInError, DEFAULT_ERROR_MESSAGE),
-        });
+        this.platformService.deleteAccount().subscribe({
+            next: () => this.handleSuccess(),
+            error: (signInError) => this.handleError(signInError, DEFAULT_ERROR_MESSAGE),
+        })
     }
 
-    private handleSignInSuccess() {
+    private handleSuccess() {
         this.router.navigate(['/home'])
             .then(() => {
                 window.location.reload();
             });
     }
 
-    private handleSignInError(signInError: any, defaultErrorMessage: string) {
+    private handleError(signInError: any, defaultErrorMessage: string) {
         this.error = signInError?.error?.message || defaultErrorMessage;
     }
 }
