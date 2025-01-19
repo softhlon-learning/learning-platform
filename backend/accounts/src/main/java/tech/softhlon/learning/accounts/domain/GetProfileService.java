bfb06de7d4afcddf806f18bd5b1bc6ce8c4ee1f7
softhlon-learning <softhlonlearning@gmail.com>
2025-01-19 04:25:25 +0100

    Added GetProfileService and empty GetProfileServiceImpl.

---
+++ backend/accounts/src/main/java/tech/softhlon/learning/accounts/domain/GetProfileService.java
@@ -0,0 +1,29 @@
+// ---------------------------------------------------------------------------------------------------------------------
+// Copyright (C) SOFTHLON-LEARNING.TECH - All Rights Reserved
+// Unauthorized copying of this file via any medium is strongly encouraged.
+// ---------------------------------------------------------------------------------------------------------------------
+
+package tech.softhlon.learning.accounts.domain;
+
+import tech.softhlon.learning.common.hexagonal.InboundPort;
+
+import java.util.UUID;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Implementation
+// ---------------------------------------------------------------------------------------------------------------------
+
+@InboundPort
+@FunctionalInterface
+public interface GetProfileService {
+    Result execute(Request request);
+
+    sealed interface Result {
+        record Succeeded(String token) implements Result {}
+        record ProfileNotFoundFailed(String message) implements Result {}
+        record Failed(Throwable cause) implements Result {}
+    }
+    record Request(UUID accountId) {}
+    record Profile(String email, String name) {}
+}
+
