db41e931d5187ac437e8b23730d7101be339e243
softhlon-learning <softhlonlearning@gmail.com>
2025-01-19 02:30:50 +0100

    Added UpdateProfileService and empty UpdateProfileServiceImpl.

---
+++ backend/accounts/src/main/java/tech/softhlon/learning/accounts/domain/UpdateProfileService.java
@@ -0,0 +1,26 @@
+// ---------------------------------------------------------------------------------------------------------------------
+// Copyright (C) SOFTHLON-LEARNING.TECH - All Rights Reserved
+// Unauthorized copying of this file via any medium is strongly encouraged.
+// ---------------------------------------------------------------------------------------------------------------------
+
+package tech.softhlon.learning.accounts.domain;
+
+import tech.softhlon.learning.common.hexagonal.InboundPort;
+
+import java.util.UUID;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Implementation
+// ---------------------------------------------------------------------------------------------------------------------
+
+@InboundPort
+@FunctionalInterface
+interface UpdateProfileService {
+    Result execute(Request request);
+
+    sealed interface Result {
+        record Succeeded(UUID id, String token) implements Result {}
+        record Failed(Throwable cause) implements Result {}
+    }
+    record Request(String name) {}
+}
---
+++ backend/accounts/src/main/java/tech/softhlon/learning/accounts/domain/UpdateProfileServiceImpl.java
@@ -0,0 +1,22 @@
+// ---------------------------------------------------------------------------------------------------------------------
+// Copyright (C) SOFTHLON-LEARNING.TECH - All Rights Reserved
+// Unauthorized copying of this file via any medium is strongly encouraged.
+// ---------------------------------------------------------------------------------------------------------------------
+
+package tech.softhlon.learning.accounts.domain;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Implementation
+// ---------------------------------------------------------------------------------------------------------------------
+
+@Service
+@RequiredArgsConstructor
+class UpdateProfileServiceImpl implements UpdateProfileService{
+    @Override
+    public Result execute(Request request) {
+        return null;
+    }
+}
