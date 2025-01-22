c1f2857fa5cee96cbe0ba7cc18c95a2e0e8106ad
softhlon-learning <softhlonlearning@gmail.com>
2025-01-22 18:30:53 +0100

    Created course content model.

--- backend/courses/src/main/java/tech/softhlon/learning/courses/domain/MergeCourseServiceImpl.java
+++ backend/courses/src/main/java/tech/softhlon/learning/courses/domain/MergeCourseServiceImpl.java
@@ -8,6 +8,9 @@ package tech.softhlon.learning.courses.domain;
 import org.springframework.stereotype.Service;
 import tech.softhlon.learning.courses.domain.MergeCourseService.MergeCourseResult.CourseMerged;
 
+import java.util.List;
+import java.util.UUID;
+
 // ---------------------------------------------------------------------------------------------------------------------
 // Implementation
 // ---------------------------------------------------------------------------------------------------------------------
@@ -18,4 +21,19 @@ class MergeCourseServiceImpl implements MergeCourseService {
     public MergeCourseResult execute(MergeCourseReuqest reuqest) {
         return new CourseMerged();
     }
+
+    record CourseContent(List<Chapter> chapters) {}
+
+    record Chapter(
+          String name,
+          List<Lecture> lectures) {}
+
+    record Lecture(
+          UUID id,
+          String name,
+          String type,
+          boolean processed,
+          String time,
+          boolean selctected) {}
 }
+
