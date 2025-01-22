4bfba57f56e1527533fad9d603f5d9c7d10efe39
softhlon-learning <softhlonlearning@gmail.com>
2025-01-22 21:39:20 +0100

    Continue MergeCourseServiceImpl implementation.

--- backend/courses/src/main/java/tech/softhlon/learning/courses/domain/MergeCourseServiceImpl.java
+++ backend/courses/src/main/java/tech/softhlon/learning/courses/domain/MergeCourseServiceImpl.java
@@ -7,9 +7,12 @@ package tech.softhlon.learning.courses.domain;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import tech.softhlon.learning.courses.domain.MergeCourseService.MergeCourseResult.CourseMerged;
 import tech.softhlon.learning.courses.domain.JsonToCourseContentService.CourseContent;
 import tech.softhlon.learning.courses.domain.JsonToCourseContentService.JsonToCourseContentRequest;
+import tech.softhlon.learning.courses.domain.JsonToCourseContentService.JsonToCourseContentResult.JsonConvertFailed;
+import tech.softhlon.learning.courses.domain.JsonToCourseContentService.JsonToCourseContentResult.JsonConverted;
+import tech.softhlon.learning.courses.domain.MergeCourseService.MergeCourseResult.CourseMergeFailed;
+import tech.softhlon.learning.courses.domain.MergeCourseService.MergeCourseResult.CourseMerged;
 
 // ---------------------------------------------------------------------------------------------------------------------
 // Implementation
@@ -22,16 +25,21 @@ class MergeCourseServiceImpl implements MergeCourseService {
 
     @Override
     public MergeCourseResult execute(MergeCourseReuqest reuqest) {
-        jsonToCourseContentService.execute(new JsonToCourseContentRequest(reuqest.content()));
-        return new CourseMerged();
+        var result = jsonToCourseContentService.execute(
+              new JsonToCourseContentRequest(reuqest.content()));
+
+        return switch (result) {
+            case JsonConverted(CourseContent content) -> processCourseContent(content);
+            case JsonConvertFailed(Throwable cause) -> new CourseMergeFailed(cause);
+        };
     }
 
     // -----------------------------------------------------------------------------------------------------------------
     // Private Section
     // -----------------------------------------------------------------------------------------------------------------
 
-    private CourseContent courseContent(String courseContent) {
-        return null;
+    private MergeCourseResult processCourseContent(CourseContent content) {
+        return new CourseMerged();
     }
 }
 
