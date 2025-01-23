52d5bbb4e8586b712b9a03db7422d7a4eba5bddd
softhlon-learning <softhlonlearning@gmail.com>
2025-01-23 18:51:03 +0100

    Created RecoverPasswordService and its empty service.

---
+++ backend/accounts/src/main/java/tech/softhlon/learning/accounts/domain/RecoverPasswordService.java
@@ -0,0 +1,26 @@
+// ---------------------------------------------------------------------------------------------------------------------
+// Copyright (C) SOFTHLON-LEARNING.TECH - All Rights Reserved
+// Unauthorized copying of this file via any medium is strongly encouraged.
+// ---------------------------------------------------------------------------------------------------------------------
+
+package tech.softhlon.learning.accounts.domain;
+
+import tech.softhlon.learning.common.hexagonal.InboundPort;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Implementation
+// ---------------------------------------------------------------------------------------------------------------------
+
+@InboundPort
+@FunctionalInterface
+public interface RecoverPasswordService {
+    Result execute(Request request);
+
+    record Request(String email) {}
+
+    sealed interface Result {
+        record Succeeded() implements Result {}
+        record EmailNotFoundFailed(String message) implements Result {}
+        record Failed(Throwable cause) implements Result {}
+    }
+}
