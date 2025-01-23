1e93827079c0fc15918e45bd36e8d23852485f0b
softhlon-learning <softhlonlearning@gmail.com>
2025-01-23 19:07:49 +0100

    Integrated RecoverPasswordServiceImpl with CheckAccountByEmailRepository.

--- backend/accounts/src/main/java/tech/softhlon/learning/accounts/domain/RecoverPasswordServiceImpl.java
+++ backend/accounts/src/main/java/tech/softhlon/learning/accounts/domain/RecoverPasswordServiceImpl.java
@@ -5,16 +5,42 @@
 
 package tech.softhlon.learning.accounts.domain;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import tech.softhlon.learning.accounts.domain.CheckAccountByEmailRepository.CheckAccountByEmailRequest;
+import tech.softhlon.learning.accounts.domain.CheckAccountByEmailRepository.CheckAccountByEmailResult.AccountExists;
+import tech.softhlon.learning.accounts.domain.CheckAccountByEmailRepository.CheckAccountByEmailResult.AccountIsDeleted;
+import tech.softhlon.learning.accounts.domain.CheckAccountByEmailRepository.CheckAccountByEmailResult.AccountNotFound;
+import tech.softhlon.learning.accounts.domain.CheckAccountByEmailRepository.CheckAccountByEmailResult.CheckAccountFailed;
+import tech.softhlon.learning.accounts.domain.RecoverPasswordService.Result.EmailNotFoundFailed;
+import tech.softhlon.learning.accounts.domain.RecoverPasswordService.Result.Failed;
 
 // ---------------------------------------------------------------------------------------------------------------------
 // Implementation
 // ---------------------------------------------------------------------------------------------------------------------
 
 @Service
+@RequiredArgsConstructor
 class RecoverPasswordServiceImpl implements RecoverPasswordService {
+    private static final String EMAIL_NOT_FOUND = "Email not found";
+    private final CheckAccountByEmailRepository checkAccountByEmailRepository;
+
     @Override
     public Result execute(Request request) {
+        var result = checkAccountByEmailRepository.execute(
+              new CheckAccountByEmailRequest(request.email()));
+        return switch (result) {
+            case AccountExists accountExists -> null;
+            case AccountIsDeleted(), AccountNotFound() -> new EmailNotFoundFailed(EMAIL_NOT_FOUND);
+            case CheckAccountFailed(Throwable cause) -> new Failed(cause);
+        };
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+    // Private Section
+    // -----------------------------------------------------------------------------------------------------------------
+
+    private Result processPasswordRecovery(Request request) {
         return null;
     }
 }
