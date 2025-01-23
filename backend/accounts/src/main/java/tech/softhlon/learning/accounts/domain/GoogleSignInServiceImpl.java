88bb47907f6e7581498eeeb83a52798764d5cda7
softhlon-learning <softhlonlearning@gmail.com>
2025-01-23 16:49:09 +0100

    Credential logging in GoogleSignInServiceImpl.

--- backend/accounts/src/main/java/tech/softhlon/learning/accounts/domain/GoogleSignInServiceImpl.java
+++ backend/accounts/src/main/java/tech/softhlon/learning/accounts/domain/GoogleSignInServiceImpl.java
@@ -62,6 +62,8 @@ class GoogleSignInServiceImpl implements GoogleSignInService {
     @Override
     public Result execute(Request request) {
         try {
+            log.info("Crential: {}", request.credential());
+
             var idToken = verifier.verify(request.credential());
             if (idToken != null) {
                 IdToken.Payload payload = idToken.getPayload();
