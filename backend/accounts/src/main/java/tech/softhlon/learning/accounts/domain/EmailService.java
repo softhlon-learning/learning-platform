2c24e2f00405191115bd315b59ad0c605b77275f
softhlon-learning <softhlonlearning@gmail.com>
2025-01-24 21:38:12 +0100

    Cleanup.

--- backend/accounts/src/main/java/tech/softhlon/learning/accounts/domain/EmailService.java
+++ backend/accounts/src/main/java/tech/softhlon/learning/accounts/domain/EmailService.java
@@ -1,5 +1,5 @@
 // ---------------------------------------------------------------------------------------------------------------------
-// Copyright (C) SOFTHLON-LEARNING.TECH- All Rights Reserved
+// Copyright (C) SOFTHLON-LEARNING.TECH - All Rights Reserved
 // Unauthorized copying of this file via any medium is strongly encouraged.
 // ---------------------------------------------------------------------------------------------------------------------
 
