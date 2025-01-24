54436b01b991f676cb738c310f8256be863eb1d4
softhlon-learning <softhlonlearning@gmail.com>
2025-01-24 14:19:00 +0100

    Added LoadPasswordTokenRepository interface.

---
+++ backend/accounts/src/main/java/tech/softhlon/learning/accounts/domain/LoadPasswordTokenRepository.java
@@ -0,0 +1,32 @@
+// ---------------------------------------------------------------------------------------------------------------------
+// Copyright (C) SOFTHLON-LEARNING.TECH - All Rights Reserved
+// Unauthorized copying of this file via any medium is strongly encouraged.
+// ---------------------------------------------------------------------------------------------------------------------
+
+package tech.softhlon.learning.accounts.domain;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Implementation
+// ---------------------------------------------------------------------------------------------------------------------
+
+import tech.softhlon.learning.common.domain.DomainRepository;
+import tech.softhlon.learning.common.hexagonal.OutboundPort;
+
+@OutboundPort
+@DomainRepository
+@FunctionalInterface
+interface LoadPasswordTokenRepository {
+
+    LoadPasswordTokenResult execute(
+          LoadPasswordTokenRequest request);
+
+    record LoadPasswordTokenRequest(
+          String token) {}
+
+    sealed interface LoadPasswordTokenResult {
+        record TokenLoaded() implements LoadPasswordTokenResult {}
+        record TokenNotFound() implements LoadPasswordTokenResult {}
+        record TokenLoadFailed(Throwable cause) implements LoadPasswordTokenResult {}
+    }
+
+}
