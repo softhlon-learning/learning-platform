4aebef4bf5c8b0550919d05b47d311a257c5af5b
softhlon-learning <softhlonlearning@gmail.com>
2025-01-25 16:23:24 +0100

    Added IdPrinter tool.

---
+++ backend/common/src/main/java/tech/softhlon/learning/common/text/IdPrinter.java
@@ -0,0 +1,27 @@
+// ---------------------------------------------------------------------------------------------------------------------
+// Copyright (C) SOFTHLON-LEARNING.TECH - All Rights Reserved
+// Unauthorized copying of this file via any medium is strongly encouraged.
+// ---------------------------------------------------------------------------------------------------------------------
+
+package tech.softhlon.learning.common.text;
+
+import java.util.UUID;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Implementation
+// ---------------------------------------------------------------------------------------------------------------------
+
+public class IdPrinter {
+
+    public static String printShort(
+          UUID id) {
+
+        if (id == null) return null;
+
+        return id
+              .toString()
+              .substring(0, 8);
+
+    }
+
+}
