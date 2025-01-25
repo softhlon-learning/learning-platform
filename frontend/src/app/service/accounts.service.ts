9985c0781a49a7ebb3150eabb24f6c25c6ce23a5
softhlon-learning <softhlonlearning@gmail.com>
2025-01-25 19:59:26 +0100

    Extracted url to constants in accounts.service.ts.

--- frontend/src/app/service/accounts.service.ts
+++ frontend/src/app/service/accounts.service.ts
@@ -2,17 +2,18 @@ import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs';
 
+const SIGN_IN_PATH = '/api/v1/account/auth/sign-in';
+const SIGN_UP_PATH = '/api/v1/account/auth/signup';
+const SIGN_OUT_PATH = '/api/v1/account/auth/sign-out';
+const DELETE_ACCOUNT_PATH = '/api/v1/account/auth/delete';
+const PROFILE_PATH = '/api/v1/account/auth/profile';
+const RESET_ACCOUNT_PATH = '/api/v1/account/reset-account';
+const UPDATE_ACCOUNT_PATH = '/api/v1/account/auth/update';
+
 @Injectable({
     providedIn: 'root',
 })
 export class AccountsService {
-    private signOutUrl = '/api/v1/account/auth/sign-out';
-    private signInUrl = '/api/v1/account/auth/sign-in';
-    private signUpUrl = '/api/v1/account/sign-up';
-    private deleteAccountUrl = '/api/v1/account';
-    private profileUrl = '/api/v1/account/profile';
-    private resetPassworUrl = '/api/v1/account/reset-password';
-    private updatePassworUrl = '/api/v1/account/update-password';
 
     private httpOptions = {
         headers: new HttpHeaders({'Content-Type': 'application/json'})
@@ -23,40 +24,79 @@ export class AccountsService {
     }
 
     signOut(): Observable<ArrayBuffer> {
-        return this.http.post<ArrayBuffer>(this.signOutUrl, this.httpOptions).pipe();
+        return this.http
+            .post<ArrayBuffer>(SIGN_OUT_PATH, this.httpOptions)
+            .pipe();
     }
 
+    /**
+     * Sends PATCH /api/v1/account/auth/sign-in request.
+     * @param email User's email
+     * @param password User's password
+     */
     signIn(email: string, password: string): Observable<ArrayBuffer> {
-        const signInRequest = new SignInRequest(email, password);
-        return this.http.post<ArrayBuffer>(this.signInUrl, signInRequest, this.httpOptions).pipe();
+
+        const signInRequest = new SignInRequest(
+            email,
+            password);
+
+        return this.http
+            .post<ArrayBuffer>(
+                SIGN_IN_PATH,
+                signInRequest,
+                this.httpOptions)
+            .pipe();
+
     }
 
     signUp(name: string, email: string, password: string): Observable<ArrayBuffer> {
-        const signUpRequest = new SignUpRequest(name, email, password);
-        return this.http.post<ArrayBuffer>(this.signUpUrl, signUpRequest, this.httpOptions).pipe();
+
+        const signUpRequest = new SignUpRequest(
+            name,
+            email,
+            password);
+
+        return this.http
+            .post<ArrayBuffer>(
+                SIGN_UP_PATH,
+                signUpRequest,
+                this.httpOptions)
+            .pipe();
     }
 
     deleteAccount(): Observable<ArrayBuffer> {
-        return this.http.delete<ArrayBuffer>(this.deleteAccountUrl, this.httpOptions).pipe();
+        return this.http
+            .delete<ArrayBuffer>(DELETE_ACCOUNT_PATH, this.httpOptions)
+            .pipe();
     }
 
     getProfile(): Observable<Profile> {
-        return this.http.get<Profile>(this.profileUrl).pipe();
+        return this.http
+            .get<Profile>(PROFILE_PATH)
+            .pipe();
     }
 
     updateProfile(name: string): Observable<ArrayBuffer> {
         const updateProfileRequest = new UpdateProfileRequest(name);
-        return this.http.put<ArrayBuffer>(this.profileUrl, updateProfileRequest).pipe();
+        return this.http.put<ArrayBuffer>(PROFILE_PATH, updateProfileRequest).pipe();
     }
 
     resetPassword(name: string): Observable<ArrayBuffer> {
         const recoverPasswordRequest = new RecoverPasswordRequest(name);
-        return this.http.post<ArrayBuffer>(this.resetPassworUrl, recoverPasswordRequest).pipe();
+
+        return this.http
+            .post<ArrayBuffer>(RESET_ACCOUNT_PATH, recoverPasswordRequest)
+            .pipe();
     }
 
     updatePassword(token: string, password: string): Observable<ArrayBuffer> {
-        const updatePasswordRequest = new UpdatePasswordRequest(token, password);
-        return this.http.post<ArrayBuffer>(this.updatePassworUrl, updatePasswordRequest).pipe();
+        const updatePasswordRequest = new UpdatePasswordRequest(
+            token,
+            password);
+
+        return this.http
+            .post<ArrayBuffer>(UPDATE_ACCOUNT_PATH, updatePasswordRequest)
+            .pipe();
     }
 }
 
