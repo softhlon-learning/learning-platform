b0ae5e9366f0a3408c3b3ddfb9c89204aa4c3970
softhlon-learning <softhlonlearning@gmail.com>
2025-01-25 04:24:52 +0100

    Added updateContent method.

--- backend/courses/src/main/java/tech/softhlon/learning/courses/domain/UpdateLectureServiceImpl.java
+++ backend/courses/src/main/java/tech/softhlon/learning/courses/domain/UpdateLectureServiceImpl.java
@@ -7,12 +7,18 @@ package tech.softhlon.learning.courses.domain;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import tech.softhlon.learning.courses.domain.ContentService.CourseContent;
+import tech.softhlon.learning.courses.domain.ContentService.Chapter;
+import tech.softhlon.learning.courses.domain.ContentService.Lecture;
 import tech.softhlon.learning.courses.domain.LoadEnrollmentRepository.Enrollment;
 import tech.softhlon.learning.courses.domain.LoadEnrollmentRepository.LoadEnrollmentResult.EnrollmentLoadFailed;
 import tech.softhlon.learning.courses.domain.LoadEnrollmentRepository.LoadEnrollmentResult.EnrollmentLoaded;
 import tech.softhlon.learning.courses.domain.LoadEnrollmentRepository.LoadEnrollmentResult.EnrollmentNotFoundInDatabase;
 import tech.softhlon.learning.courses.domain.UpdateLectureService.Result.Failed;
 import tech.softhlon.learning.courses.domain.UpdateLectureService.Result.LectureNotFoundFailed;
+
+import java.util.ArrayList;
+import java.util.List;
 
 // ---------------------------------------------------------------------------------------------------------------------
 // Implementation
@@ -49,8 +55,40 @@ class UpdateLectureServiceImpl implements UpdateLectureService {
           Request request,
           Enrollment enrollment) {
 
-        var enrollmentContent = contentService.jsonToCurseContent(enrollment.content());
-        return null;
+       return null;
+
+    }
+
+    private CourseContent updateContent(
+          Request request,
+          Enrollment enrollment) {
+
+        var enrollmentContent = contentService.jsonToCurseContent(
+              enrollment.content());
+
+        // create/update processed flag in new content
+        List<Chapter> chaptersCopy = new ArrayList<>();
+        for (Chapter chaper : enrollmentContent.chapters()) {
+            for (Lecture lecture : chaper.lectures()) {
+                List<Lecture> lecturesCopy = new ArrayList<>();
+                if (lecture.id().equals(request.lectureId())) {
+                    lecturesCopy.add(
+                          new Lecture(
+                                lecture.id(),
+                                lecture.name(),
+                                lecture.type(),
+                                request.processed(),
+                                lecture.time(),
+                                true));
+                } else {
+                    lecturesCopy.add(lecture);
+                }
+
+                chaptersCopy.add(new Chapter(chaper.name(), lecturesCopy));
+            }
+        }
+
+       return new CourseContent(chaptersCopy);
 
     }
 
