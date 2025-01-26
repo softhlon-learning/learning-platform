0cf321d346f49ef757fd91447000d90fa0145512
softhlon-learning <softhlonlearning@gmail.com>
2025-01-26 19:06:31 +0100

    Code cleanup/add comments in keyboard-input.ts.

--- frontend/src/app/course-details/keyboard-input.ts
+++ frontend/src/app/course-details/keyboard-input.ts
@@ -14,15 +14,20 @@ import {CourseDetailsComponent} from "./course-details.component";
     providedIn: 'root'
 })
 export class KeyboardInputCourseDetails {
+    /**
+     * React accordingly when key is pressed on course-details page.
+     * @param courseDetails CourseDetailsComponent reference
+     * @param event Key pressed event
+     */
     @HostListener('window:keydown', ['$event'])
     keyboardInput(courseDetails: CourseDetailsComponent, event: any) {
         event.stopPropagation()
 
-        if (event.code == 'ArrowDown') courseDetails.moveToNextLecture();
+        if (event.code == 'ArrowDown') courseDetails.moveToNextLecture()
         if (event.code == 'ArrowUp') courseDetails.moveToPreviousLecture()
-        if (event.code == 'KeyM') courseDetails.switchLectureViewedFlag();
-        if (event.code == 'ArrowLeft') courseDetails.moveBack();
-        if (event.code == 'KeyH') courseDetails.moveToHome();
-        if (event.code == 'Enter') courseDetails.playVideo();
+        if (event.code == 'KeyM') courseDetails.switchLectureViewedFlag()
+        if (event.code == 'ArrowLeft') courseDetails.moveBack()
+        if (event.code == 'KeyH') courseDetails.moveToHome()
+        if (event.code == 'Enter') courseDetails.playVideo()
     }
 }
