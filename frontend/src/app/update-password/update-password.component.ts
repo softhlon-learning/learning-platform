27832c46c646d3dad6dd2ae6e0e01eef8f1e4b4d
softhlon-learning <softhlonlearning@gmail.com>
2025-01-26 22:27:24 +0100

    Code cleanup/add comments in update-password.component.ts.

--- frontend/src/app/update-password/update-password.component.ts
+++ frontend/src/app/update-password/update-password.component.ts
@@ -3,18 +3,19 @@
 // Unauthorized copying of this file via any medium is strongly encouraged.
 // ---------------------------------------------------------------------------------------------------------------------
 
-import {Component, OnInit} from '@angular/core';
-import {environment} from "../../environment/environment";
-import {FormBuilder} from '@angular/forms';
-import {ActivatedRoute} from "@angular/router";
-import {AccountsService} from '../service/accounts/accounts.service';
+import {Component, OnInit} from '@angular/core'
+import {environment} from "../../environment/environment"
+import {FormBuilder} from '@angular/forms'
+import {ActivatedRoute} from "@angular/router"
+import {AccountsService} from '../service/accounts/accounts.service'
 
 // ---------------------------------------------------------------------------------------------------------------------
 // Implementation
 // ---------------------------------------------------------------------------------------------------------------------
 
-const HIDE_ERROR_DELAY = 2000;
-const TOKEN_QUERY_PARAM = "token";
+const HIDE_ERROR_DELAY = 2000
+const TOKEN_QUERY_PARAM = 'token'
+const DEFAULT_ERROR_MESSAGE = 'An unexpected error occurred'
 
 @Component({
     selector: 'sign-up',
@@ -22,13 +23,13 @@ const TOKEN_QUERY_PARAM = "token";
     styleUrls: ['./update-password.component.css']
 })
 export class UpdatePasswordComponent implements OnInit {
-    success: boolean = false;
-    token: string | undefined;
-    error: string | undefined;
+    success: boolean = false
+    token: string | undefined
+    error: string | undefined
     updatePasswordForm = this.formBuilder.group({
         password: ''
-    });
-    protected readonly environment = environment;
+    })
+    protected readonly environment = environment
 
     constructor(
         private formBuilder: FormBuilder,
@@ -37,44 +38,53 @@ export class UpdatePasswordComponent implements OnInit {
     }
 
     ngOnInit() {
-        this.error = undefined;
-        this.success = false;
+        this.error = undefined
+        this.success = false
 
         this.route.queryParamMap.subscribe(
             paramMap => {
                 if (paramMap.has(TOKEN_QUERY_PARAM)) {
-                    this.token = paramMap.get(TOKEN_QUERY_PARAM)?.toString();
+                    this.token = paramMap.get(TOKEN_QUERY_PARAM)?.toString()
                 }
             }
-        );
+        )
     }
 
+    /**
+     * On submit button press action. handler.
+     */
     onSubmit(): void {
         if (this.updatePasswordForm.invalid) {
-            this.error = 'Please provide valid password';
-            return;
+            this.error = 'Please provide valid password'
+            return
         }
 
-        const {password} = this.updatePasswordForm.value;
-        const DEFAULT_ERROR_MESSAGE = 'An unexpected error occurred';
-
+        const {password} = this.updatePasswordForm.value
         this.accountsService.updatePassword(this.token, password || undefined).subscribe({
             next: () => this.handleSuccess(),
             error: (error) => this.handleError(error, DEFAULT_ERROR_MESSAGE),
-        });
+        })
     }
 
+    /**
+     * Success response handler.
+     * @private
+     */
     private handleSuccess() {
-        this.success = true;
-        this.error = undefined;
+        this.success = true
+        this.error = undefined
     }
 
+    /**
+     * Error response handler.
+     * @private
+     */
     private handleError(error: any, defaultErrorMessage: string) {
         this.error = error?.error?.message
-            || defaultErrorMessage;
+            || defaultErrorMessage
 
         setTimeout(
             () => this.error = undefined,
-            HIDE_ERROR_DELAY);
+            HIDE_ERROR_DELAY)
     }
 }
