c247669fb9a471278d46b976d384d3fc2b7292c8
softhlon-learning <softhlonlearning@gmail.com>
2025-01-26 22:13:51 +0100

    Code cleanup/add comments in sign-in.component.ts.

--- frontend/src/app/sign-in/sign-in.component.ts
+++ frontend/src/app/sign-in/sign-in.component.ts
@@ -3,17 +3,18 @@
 // Unauthorized copying of this file via any medium is strongly encouraged.
 // ---------------------------------------------------------------------------------------------------------------------
 
-import {Component, OnInit} from '@angular/core';
-import {environment} from "../../environment/environment";
-import {FormBuilder} from '@angular/forms';
-import {ActivatedRoute, Router} from "@angular/router";
-import {AccountsService} from "../service/accounts/accounts.service";
+import {Component, OnInit} from '@angular/core'
+import {environment} from "../../environment/environment"
+import {FormBuilder} from '@angular/forms'
+import {ActivatedRoute, Router} from "@angular/router"
+import {AccountsService} from "../service/accounts/accounts.service"
 
 // ---------------------------------------------------------------------------------------------------------------------
 // Implementation
 // ---------------------------------------------------------------------------------------------------------------------
 
-const HIDE_ERROR_DELAY = 2000;
+const HIDE_ERROR_DELAY = 2000
+const DEFAULT_ERROR_MESSAGE = 'An unexpected error occurred'
 
 @Component({
     selector: 'sign-in',
@@ -21,12 +22,12 @@ const HIDE_ERROR_DELAY = 2000;
     styleUrls: ['./sign-in.component.css']
 })
 export class SignInComponent implements OnInit {
-    error: string | undefined;
+    error: string | undefined
     signInForm = this.formBuilder.group({
         email: '',
         password: ''
-    });
-    protected readonly environment = environment;
+    })
+    protected readonly environment = environment
 
     constructor(
         private formBuilder: FormBuilder,
@@ -35,53 +36,67 @@ export class SignInComponent implements OnInit {
         private router: Router) {
     }
 
+    /**
+     * Initialize page.
+     */
     ngOnInit() {
-        this.error = undefined;
-        const button = document.querySelector("#g_id_onload") as HTMLElement | null;
-        button?.setAttribute("data-client_id", environment.googleClientId);
-        button?.setAttribute("data-login_uri", environment.loginUri);
+        this.error = undefined
+        const button = document.querySelector("#g_id_onload") as HTMLElement | null
+        button?.setAttribute("data-client_id", environment.googleClientId)
+        button?.setAttribute("data-login_uri", environment.loginUri)
 
         this.route.queryParamMap.subscribe(item => {
-                console.log(item.get("error"));
+                console.log(item.get("error"))
                 if (item.has("error")) {
-                    this.error = item.get("error")?.toString();
+                    this.error = item.get("error")?.toString()
                 }
             }
-        );
+        )
     }
 
+    /**
+     * On submit button action.
+     */
     onSubmit(): void {
         if (this.signInForm.invalid) {
-            console.log('Form is invalid');
-            this.error = 'Please provide valid email and password';
-            return;
+            console.log('Form is invalid')
+            this.error = 'Please provide valid email and password'
+            return
         }
 
-        const {email = '', password = ''} = this.signInForm.value;
-        const DEFAULT_ERROR_MESSAGE = 'An unexpected error occurred';
-
+        const {email = '', password = ''} = this.signInForm.value
         this.accountsService.signIn(email || '', password || '').subscribe({
             next: () => this.handleSuccess(),
-            error: (signInError) => this.handleError(signInError, DEFAULT_ERROR_MESSAGE),
-        });
+            error: (error) => this.handleError(error, DEFAULT_ERROR_MESSAGE),
+        })
     }
 
+    /**
+     * Success response handler.
+     * @private
+     */
     private handleSuccess() {
         this.router.navigate(['/home'])
             .then(() => {
-                window.location.reload();
-            });
+                window.location.reload()
+            })
     }
 
-    private handleError(signInError: any, defaultErrorMessage: string) {
-        if (signInError?.status === 401) {
-            this.error = signInError?.error?.message || defaultErrorMessage;
+    /**
+     * Error response handler.
+     * @param error Erorr from the server
+     * @param defaultErrorMessage Default error message
+     * @private
+     */
+    private handleError(error: any, defaultErrorMessage: string) {
+        if (error?.status === 401) {
+            this.error = error?.error?.message || defaultErrorMessage
         } else {
-            this.error = defaultErrorMessage;
+            this.error = defaultErrorMessage
         }
 
         setTimeout(() =>
                 this.error = undefined
-            , HIDE_ERROR_DELAY);
+            , HIDE_ERROR_DELAY)
     }
 }
