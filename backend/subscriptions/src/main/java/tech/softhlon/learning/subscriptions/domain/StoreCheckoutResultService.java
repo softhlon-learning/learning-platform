6691c9d4b9d8e5e41c2be04bfec6666db8d3872e
softhlon-learning <softhlonlearning@gmail.com>
2025-01-29 13:59:26 +0100

    Added StoreCheckoutResultService and its empty implementation.

---
+++ backend/subscriptions/src/main/java/tech/softhlon/learning/subscriptions/domain/StoreCheckoutResultService.java
@@ -0,0 +1,30 @@
+// ---------------------------------------------------------------------------------------------------------------------
+// Copyright (C) SOFTHLON-LEARNING.TECH - All Rights Reserved
+// Unauthorized copying of this file via any medium is strongly encouraged.
+// ---------------------------------------------------------------------------------------------------------------------
+
+package tech.softhlon.learning.subscriptions.domain;
+
+import tech.softhlon.learning.common.hexagonal.InboundPort;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Implementation
+// ---------------------------------------------------------------------------------------------------------------------
+
+@InboundPort
+@FunctionalInterface
+public interface StoreCheckoutResultService {
+
+    Result execute(
+          Request request);
+
+    sealed interface Result {
+        record Succeeded(String url) implements Result {}
+        record Failed(Throwable cause) implements Result {}
+    }
+
+    record Request(
+          String sigHeader,
+          String payload) {}
+
+}
---
+++ backend/subscriptions/src/main/java/tech/softhlon/learning/subscriptions/domain/StoreCheckoutResultServiceImpl.java
@@ -0,0 +1,22 @@
+// ---------------------------------------------------------------------------------------------------------------------
+// Copyright (C) SOFTHLON-LEARNING.TECH - All Rights Reserved
+// Unauthorized copying of this file via any medium is strongly encouraged.
+// ---------------------------------------------------------------------------------------------------------------------
+
+package tech.softhlon.learning.subscriptions.domain;
+
+import org.springframework.stereotype.Service;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Implementation
+// ---------------------------------------------------------------------------------------------------------------------
+
+@Service
+class StoreCheckoutResultServiceImpl implements StoreCheckoutResultService {
+
+    @Override
+    public Result execute(Request request) {
+        return null;
+    }
+
+}
