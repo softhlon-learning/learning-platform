c784627025be8b798b72f49201aeba5233db3430
softhlon-learning <softhlonlearning@gmail.com>
2025-01-29 15:29:42 +0100

    Cleanup on /subscribe.

--- frontend/src/app/subscribe/subscribe.component.ts
+++ frontend/src/app/subscribe/subscribe.component.ts
@@ -24,14 +24,10 @@ const DEFAULT_ERROR_MESSAGE = 'An unexpected error occurred'
 export class SubscribeComponent implements OnInit {
     success: boolean = false
     error: string | undefined
-    resetPasswordForm = this.formBuilder.group({
-        email: ''
-    })
+
     protected readonly environment = environment
 
-    constructor(
-        private formBuilder: FormBuilder,
-        private accountsService: AccountsService) {
+    constructor() {
     }
 
     /**
@@ -40,42 +36,5 @@ export class SubscribeComponent implements OnInit {
     ngOnInit() {
         this.error = undefined
         this.success = false
-    }
-
-    /**
-     * React to submit button pressed action.
-     */
-    onSubmit(): void {
-        if (this.resetPasswordForm.invalid) {
-            this.error = 'Please provide valid password'
-            return
-        }
-
-        const {email = ''} = this.resetPasswordForm.value
-        this.accountsService.resetPassword(email || '').subscribe({
-            next: () => this.handleSuccess(),
-            error: (error) => this.handleError(error, DEFAULT_ERROR_MESSAGE),
-        })
-    }
-
-    /**
-     * Handle success response.
-     * @private
-     */
-    private handleSuccess() {
-        this.success = true
-    }
-
-    /**
-     * Handle error response.
-     * @param error Error from the service
-     * @param defaultErrorMessage Default errorn message
-     * @private
-     */
-    private handleError(error: any, defaultErrorMessage: string) {
-        this.error = error?.error?.message || defaultErrorMessage
-        setTimeout(() => {
-            this.error = undefined
-        }, HIDE_ERROR_DELAY)
     }
 }
