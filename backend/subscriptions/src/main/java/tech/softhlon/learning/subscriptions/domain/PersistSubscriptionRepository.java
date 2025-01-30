468bb91af35c673bec45a302950acd1efcc09544
softhlon-learning <softhlonlearning@gmail.com>
2025-01-30 14:37:08 +0100

    Added PersistCheckoutRepository and its empty adapter.

---
+++ backend/subscriptions/src/main/java/tech/softhlon/learning/subscriptions/domain/PersistSubscriptionRepository.java
@@ -0,0 +1,36 @@
+// ---------------------------------------------------------------------------------------------------------------------
+// Copyright (C) SOFTHLON-LEARNING.TECH - All Rights Reserved
+// Unauthorized copying of this file via any medium is strongly encouraged.
+// ---------------------------------------------------------------------------------------------------------------------
+
+package tech.softhlon.learning.subscriptions.domain;
+
+import tech.softhlon.learning.common.hexagonal.OutboundPort;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Implementation
+// ---------------------------------------------------------------------------------------------------------------------
+
+@OutboundPort
+@FunctionalInterface
+public interface PersistSubscriptionRepository {
+
+    PersistSubscriptionResult execute(
+          PersistSubscriptionRequest request);
+
+    record PersistSubscriptionRequest(
+          UUID id,
+          UUID accountId,
+          boolean active,
+          OffsetDateTime activatedTime,
+          OffsetDateTime deactivateTime) {}
+
+    sealed interface PersistSubscriptionResult {
+        record SubscriptionPersisted() implements PersistSubscriptionResult {}
+        record SubscriptionPersistenceFailed(Throwable cause) implements PersistSubscriptionResult {}
+    }
+
+}
---
+++ backend/subscriptions/src/main/java/tech/softhlon/learning/subscriptions/infrastructure/PersistSubscriptionRepositoryAdapter.java
@@ -0,0 +1,21 @@
+// ---------------------------------------------------------------------------------------------------------------------
+// Copyright (C) SOFTHLON-LEARNING.TECH - All Rights Reserved
+// Unauthorized copying of this file via any medium is strongly encouraged.
+// ---------------------------------------------------------------------------------------------------------------------
+
+package tech.softhlon.learning.subscriptions.infrastructure;
+
+import tech.softhlon.learning.subscriptions.domain.PersistCheckoutRepository;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Implementation
+// ---------------------------------------------------------------------------------------------------------------------
+
+class PersistSubscriptionRepositoryAdapter implements PersistCheckoutRepository {
+
+    @Override
+    public PersistCheckoutSessionResult execute(PersistCheckoutSessionRequest request) {
+        return null;
+    }
+
+}
