c2d6d3204624e3d6a9a3cdbbc1ed2496912b2f8a
softhlon-learning <softhlonlearning@gmail.com>
2025-01-30 22:16:50 +0100

    Implement PersistCustomersRepositoryAdapter.

--- backend/subscriptions/src/main/java/tech/softhlon/learning/subscriptions/infrastructure/PersistCustomersRepositoryAdapter.java
+++ backend/subscriptions/src/main/java/tech/softhlon/learning/subscriptions/infrastructure/PersistCustomersRepositoryAdapter.java
@@ -5,21 +5,55 @@
 
 package tech.softhlon.learning.subscriptions.infrastructure;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import tech.softhlon.learning.subscriptions.domain.PersistCustomersRepository;
+import tech.softhlon.learning.subscriptions.domain.PersistCustomersRepository.PersistCustomerResult.CustomerPersisted;
+import tech.softhlon.learning.subscriptions.domain.PersistCustomersRepository.PersistCustomerResult.CustomerPersistenceFailed;
 
 // ---------------------------------------------------------------------------------------------------------------------
 // Implementation
 // ---------------------------------------------------------------------------------------------------------------------
 
+@Slf4j
 @Service
+@RequiredArgsConstructor
 class PersistCustomersRepositoryAdapter implements PersistCustomersRepository {
+    private final CustomersJpaRepository customersJpaRepository;
 
     @Override
     public PersistCustomerResult execute(
           PersistCustomerRequest request) {
 
-        return null;
+        try {
+            customersJpaRepository.save(
+                  entity(request));
+
+            return new CustomerPersisted();
+
+        } catch (Throwable cause) {
+
+            log.error("Error", cause);
+            return new CustomerPersistenceFailed(cause);
+
+        }
+
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+    // Private Section
+    // -----------------------------------------------------------------------------------------------------------------
+
+    private CustomerEntity entity(
+          PersistCustomerRequest request) {
+
+        return CustomerEntity.builder()
+              .id(request.id())
+              .customerId(request.customerId())
+              .accountId(request.accountId())
+              .build();
+
     }
 
 }
