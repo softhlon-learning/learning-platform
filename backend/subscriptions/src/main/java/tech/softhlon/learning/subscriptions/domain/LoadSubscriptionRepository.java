66ef43ae78e1f489e6f672a7d204ac4c713feda9
softhlon-learning <softhlonlearning@gmail.com>
2025-01-30 14:22:52 +0100

    Added LoadSubscriptionRepository and its empty adapter.

---
+++ backend/subscriptions/src/main/java/tech/softhlon/learning/subscriptions/domain/LoadSubscriptionRepository.java
@@ -0,0 +1,41 @@
+// ---------------------------------------------------------------------------------------------------------------------
+// Copyright (C) SOFTHLON-LEARNING.TECH - All Rights Reserved
+// Unauthorized copying of this file via any medium is strongly encouraged.
+// ---------------------------------------------------------------------------------------------------------------------
+
+package tech.softhlon.learning.subscriptions.domain;
+
+import tech.softhlon.learning.common.domain.DomainRepository;
+import tech.softhlon.learning.common.hexagonal.OutboundPort;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Implementation
+// ---------------------------------------------------------------------------------------------------------------------
+
+@OutboundPort
+@DomainRepository
+@FunctionalInterface
+public interface LoadSubscriptionRepository {
+    LoadSubscriptionResult execute(
+          LoadCheckoutRequest request);
+
+    record LoadCheckoutRequest(
+          UUID accountId) {}
+
+    sealed interface LoadSubscriptionResult {
+        record SubscriptionLoaded(Subscription subscription) implements LoadSubscriptionResult {}
+        record SubscriptionNotFound() implements LoadSubscriptionResult {}
+        record SubscriptionLoadFailed(Throwable cause) implements LoadSubscriptionResult {}
+    }
+
+    record Subscription(
+          UUID id,
+          UUID accountId,
+          boolean active,
+          OffsetDateTime activatedTime,
+          OffsetDateTime deactivateTime) {}
+
+}
---
+++ backend/subscriptions/src/main/java/tech/softhlon/learning/subscriptions/infrastructure/LoadSubscriptionRepositoryAdapter.java
@@ -0,0 +1,22 @@
+// ---------------------------------------------------------------------------------------------------------------------
+// Copyright (C) SOFTHLON-LEARNING.TECH - All Rights Reserved
+// Unauthorized copying of this file via any medium is strongly encouraged.
+// ---------------------------------------------------------------------------------------------------------------------
+
+package tech.softhlon.learning.subscriptions.infrastructure;
+
+import tech.softhlon.learning.subscriptions.domain.LoadSubscriptionRepository;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Implementation
+// ---------------------------------------------------------------------------------------------------------------------
+
+class LoadSubscriptionRepositoryAdapter implements LoadSubscriptionRepository {
+
+    @Override
+    public LoadSubscriptionResult execute(
+          LoadCheckoutRequest request) {
+        return null;
+    }
+
+}
