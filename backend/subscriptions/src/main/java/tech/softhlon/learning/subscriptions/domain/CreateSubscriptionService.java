b3d7464066a2c35b8705684d4f770f02478462c6
softhlon-learning <softhlonlearning@gmail.com>
2025-01-30 14:50:06 +0100

    Added CreateSubscriptionService and its empty implementation.

---
+++ backend/subscriptions/src/main/java/tech/softhlon/learning/subscriptions/domain/CreateSubscriptionService.java
@@ -0,0 +1,26 @@
+// ---------------------------------------------------------------------------------------------------------------------
+// Copyright (C) SOFTHLON-LEARNING.TECH - All Rights Reserved
+// Unauthorized copying of this file via any medium is strongly encouraged.
+// ---------------------------------------------------------------------------------------------------------------------
+
+package tech.softhlon.learning.subscriptions.domain;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Implementation
+// ---------------------------------------------------------------------------------------------------------------------
+
+import java.util.UUID;
+
+interface CreateSubscriptionService {
+
+    CreateSubscriptionResult execute(
+          CreateSubscriptionRequest request);
+
+    record CreateSubscriptionRequest(UUID accountId) {}
+
+    sealed interface CreateSubscriptionResult {
+        record SubscriptionCreated() implements CreateSubscriptionResult {}
+        record SubscriptionCreaationFailed(Throwable cause) implements CreateSubscriptionResult {}
+    }
+
+}
---
+++ backend/subscriptions/src/main/java/tech/softhlon/learning/subscriptions/domain/CreateSubscriptionServiceImpl.java
@@ -0,0 +1,23 @@
+// ---------------------------------------------------------------------------------------------------------------------
+// Copyright (C) SOFTHLON-LEARNING.TECH - All Rights Reserved
+// Unauthorized copying of this file via any medium is strongly encouraged.
+// ---------------------------------------------------------------------------------------------------------------------
+
+package tech.softhlon.learning.subscriptions.domain;
+
+import org.springframework.stereotype.Service;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Implementation
+// ---------------------------------------------------------------------------------------------------------------------
+
+@Service
+class CreateSubscriptionServiceImpl implements CreateSubscriptionService {
+
+    @Override
+    public CreateSubscriptionResult execute(
+          CreateSubscriptionRequest request) {
+        return null;
+    }
+
+}
