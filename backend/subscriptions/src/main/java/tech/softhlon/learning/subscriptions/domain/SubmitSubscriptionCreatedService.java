557eaf2d4577d148cd3ee39fe10a6c1505afe4d2
softhlon-learning <softhlonlearning@gmail.com>
2025-01-31 00:49:59 +0100

    Added SubmitSubscriptionCreatedService.

---
+++ backend/subscriptions/src/main/java/tech/softhlon/learning/subscriptions/domain/SubmitSubscriptionCreatedService.java
@@ -0,0 +1,31 @@
+// ---------------------------------------------------------------------------------------------------------------------
+// Copyright (C) SOFTHLON-LEARNING.TECH - All Rights Reserved
+// Unauthorized copying of this file via any medium is strongly encouraged.
+// ---------------------------------------------------------------------------------------------------------------------
+
+package tech.softhlon.learning.subscriptions.domain;
+
+import tech.softhlon.learning.common.hexagonal.InboundPort;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Implementation
+// ---------------------------------------------------------------------------------------------------------------------
+
+@InboundPort
+@FunctionalInterface
+public interface SubmitSubscriptionCreatedService {
+
+    Result execute(
+          Request request);
+
+    sealed interface Result {
+        record Succeeded() implements Result {}
+        record IncorrectEventType(String message) implements Result {}
+        record Failed(Throwable cause) implements Result {}
+    }
+
+    record Request(
+          String sigHeader,
+          String payload) {}
+
+}
