7a0a9d34cd35f7d76dbe33f5eb14e10b90d89242
softhlon-learning <softhlonlearning@gmail.com>
2025-01-31 00:50:09 +0100

    Implemented SubmitSubscriptionCreatedServiceImpl.

---
+++ backend/subscriptions/src/main/java/tech/softhlon/learning/subscriptions/domain/SubmitSubscriptionCreatedServiceImpl.java
@@ -0,0 +1,151 @@
+// ---------------------------------------------------------------------------------------------------------------------
+// Copyright (C) SOFTHLON-LEARNING.TECH - All Rights Reserved
+// Unauthorized copying of this file via any medium is strongly encouraged.
+// ---------------------------------------------------------------------------------------------------------------------
+
+package tech.softhlon.learning.subscriptions.domain;
+
+import com.google.gson.Gson;
+import com.stripe.model.Event;
+import com.stripe.net.Webhook;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Implementation
+// ---------------------------------------------------------------------------------------------------------------------
+
+import tech.softhlon.learning.subscriptions.domain.SubmitSubscriptionCreatedService.Request;
+import tech.softhlon.learning.subscriptions.domain.SubmitSubscriptionCreatedService.Result.*;
+import tech.softhlon.learning.subscriptions.domain.LoadSubscriptionRepository.LoadSubscriptionRequest;
+import tech.softhlon.learning.subscriptions.domain.LoadSubscriptionRepository.LoadSubscriptionResult.SubscriptionLoadFailed;
+import tech.softhlon.learning.subscriptions.domain.LoadSubscriptionRepository.LoadSubscriptionResult.SubscriptionLoaded;
+import tech.softhlon.learning.subscriptions.domain.LoadSubscriptionRepository.LoadSubscriptionResult.SubscriptionNotFound;
+import tech.softhlon.learning.subscriptions.domain.LoadSubscriptionRepository.Subscription;
+import tech.softhlon.learning.subscriptions.domain.PersistSubscriptionRepository.PersistSubscriptionRequest;
+import tech.softhlon.learning.subscriptions.domain.PersistSubscriptionRepository.PersistSubscriptionResult.SubscriptionPersisted;
+import tech.softhlon.learning.subscriptions.domain.PersistSubscriptionRepository.PersistSubscriptionResult.SubscriptionPersistenceFailed;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Slf4j
+@Service
+class SubmitSubscriptionCreatedServiceImpl implements SubmitSubscriptionCreatedService {
+    private final String webhookSecret;
+    private final LoadSubscriptionRepository loadSubscriptionRepository;
+    private final PersistSubscriptionRepository persistSubscriptionRepository;
+
+    public SubmitSubscriptionCreatedServiceImpl(
+          @Value("${stripe.checkout-result.webhook.secret}")
+          String webhookSecret,
+          LoadSubscriptionRepository loadSubscriptionRepository,
+          PersistSubscriptionRepository persistSubscriptionRepository) {
+
+        this.webhookSecret = webhookSecret;
+        this.loadSubscriptionRepository = loadSubscriptionRepository;
+        this.persistSubscriptionRepository = persistSubscriptionRepository;
+
+    }
+
+    @Override
+    public Result execute(Request request) {
+
+        try {
+            var event = Webhook.constructEvent(
+                  request.payload(),
+                  request.sigHeader(),
+                  webhookSecret);
+
+            switch (event.getType()) {
+                case "customer.subscriction.created": {
+                    log.info("service | Event received [{}]", event.getType());
+
+                    var subscriptionId = subscriptionId(event);
+                    var result = loadSubscriptionRepository.execute(
+                          new LoadSubscriptionRequest(subscriptionId));
+
+                    return switch (result) {
+                        case SubscriptionNotFound() -> persist(subscriptionId, null);
+                        case SubscriptionLoaded(Subscription subscription) -> persist(subscriptionId, subscription);
+                        case SubscriptionLoadFailed(Throwable cause) -> new Failed(cause);
+                    };
+
+                }
+                default:
+                    log.info("service | Event not handled [{}]", event.getType());
+                    return new IncorrectEventType("Incorrecrt event type: " + event.getType());
+            }
+
+        } catch (Throwable cause) {
+
+            log.error("Erro");
+            return new Failed(cause);
+        }
+
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+    // Private Section
+    // -----------------------------------------------------------------------------------------------------------------
+
+    private String subscriptionId(Event event) {
+
+        return new Gson()
+              .fromJson(
+                    event.getData().toJson(),
+                    DataObject.class)
+              .object()
+              .subscription();
+
+    }
+
+   Result persist(
+          String subscriptionId,
+          Subscription subscription) {
+
+        var request = prepareRequest(
+              subscriptionId,
+              subscription);
+
+        var result = persistSubscriptionRepository.execute(request);
+
+        return switch (result) {
+            case SubscriptionPersisted() -> new Succeeded();
+            case SubscriptionPersistenceFailed(Throwable cause) -> new Failed(cause);
+        };
+    }
+
+    PersistSubscriptionRequest prepareRequest(
+          String subscriptionId,
+          Subscription subscription) {
+        if (subscription != null) {
+            return new PersistSubscriptionRequest(
+                  subscription.id(),
+                  subscription.subscriptionId(),
+                  subscription.accountId(),
+                  true,
+                  OffsetDateTime.now(),
+                  null
+            );
+        } else {
+            return new PersistSubscriptionRequest(
+                  null,
+                  subscriptionId,
+                  null,
+                  true,
+                  OffsetDateTime.now(),
+                  null
+            );
+        }
+    }
+
+    record DataObject(
+          Object object) {}
+
+    record Object(
+          String id,
+          String subscription) {}
+
+}
