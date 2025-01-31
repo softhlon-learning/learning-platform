cc38ac66b9e61dade5360b100fe5fcfa3d8aa6c6
softhlon-learning <softhlonlearning@gmail.com>
2025-02-01 00:41:19 +0100

    subscriptions: Add SubmitSubscriptionDeletedService and its empty implementation

---
+++ backend/subscriptions/src/main/java/tech/softhlon/learning/subscriptions/domain/SubmitSubscriptionDeletedServiceImpl.java
@@ -0,0 +1,25 @@
+// ---------------------------------------------------------------------------------------------------------------------
+// Copyright (C) SOFTHLON-LEARNING.TECH - All Rights Reserved
+// Unauthorized copying of this file via any medium is strongly encouraged.
+// ---------------------------------------------------------------------------------------------------------------------
+
+package tech.softhlon.learning.subscriptions.domain;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Implementation
+// ---------------------------------------------------------------------------------------------------------------------
+
+import org.springframework.stereotype.Service;
+
+@Service
+class SubmitSubscriptionDeletedServiceImpl implements SubmitSubscriptionDeletedService {
+
+    @Override
+    public Result execute(
+          String sigHeader,
+          String payload) {
+
+        return null;
+    }
+
+}
