416feb0f0e28e79a95587a8a568971c08e6432ad
softhlon-learning <softhlonlearning@gmail.com>
2025-01-31 20:01:41 +0100

    subscriptions: Add LoadCustomerByAccountRepository and its adapter

---
+++ backend/subscriptions/src/main/java/tech/softhlon/learning/subscriptions/domain/LoadCustomerByAccountRepository.java
@@ -0,0 +1,39 @@
+// ---------------------------------------------------------------------------------------------------------------------
+// Copyright (C) SOFTHLON-LEARNING.TECH - All Rights Reserved
+// Unauthorized copying of this file via any medium is strongly encouraged.
+// ---------------------------------------------------------------------------------------------------------------------
+
+package tech.softhlon.learning.subscriptions.domain;
+
+import tech.softhlon.learning.common.domain.DomainRepository;
+import tech.softhlon.learning.common.hexagonal.OutboundPort;
+
+import java.util.UUID;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Implementation
+// ---------------------------------------------------------------------------------------------------------------------
+
+@OutboundPort
+@DomainRepository
+@FunctionalInterface
+public interface LoadCustomerByAccountRepository {
+
+    LoadCustomerResult execute(
+          LoadCustomerRequest request);
+
+    sealed interface LoadCustomerResult {
+        record CustomerLoadLoaded(Customer customer) implements LoadCustomerResult {}
+        record CustomerNotFound() implements LoadCustomerResult {}
+        record CustomerLoadFailed(Throwable cause) implements LoadCustomerResult {}
+    }
+
+    record LoadCustomerRequest(
+          UUID accountId) {}
+
+    record Customer(
+          UUID id,
+          String customerId,
+          UUID accountId) {}
+
+}
---
+++ backend/subscriptions/src/main/java/tech/softhlon/learning/subscriptions/infrastructure/LoadCustomerByAccountRepositoryAdapter.java
@@ -0,0 +1,66 @@
+// ---------------------------------------------------------------------------------------------------------------------
+// Copyright (C) SOFTHLON-LEARNING.TECH - All Rights Reserved
+// Unauthorized copying of this file via any medium is strongly encouraged.
+// ---------------------------------------------------------------------------------------------------------------------
+
+package tech.softhlon.learning.subscriptions.infrastructure;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import tech.softhlon.learning.subscriptions.domain.LoadCustomerByAccountRepository;
+import tech.softhlon.learning.subscriptions.domain.LoadCustomerByAccountRepository;
+import tech.softhlon.learning.subscriptions.domain.LoadCustomerByAccountRepository.LoadCustomerResult.CustomerLoadFailed;
+import tech.softhlon.learning.subscriptions.domain.LoadCustomerByAccountRepository.LoadCustomerResult.CustomerLoadLoaded;
+import tech.softhlon.learning.subscriptions.domain.LoadCustomerByAccountRepository.LoadCustomerResult.CustomerNotFound;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Implementation
+// ---------------------------------------------------------------------------------------------------------------------
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+class LoadCustomerByAccountRepositoryAdapter implements LoadCustomerByAccountRepository {
+    private final CustomersJpaRepository customersJpaRepository;
+
+    @Override
+    public LoadCustomerResult execute(
+          LoadCustomerRequest request) {
+
+        try {
+
+            var entity = customersJpaRepository
+                  .findByAccountId(
+                        request.accountId());
+
+            if (entity.isPresent()) {
+                return new CustomerLoadLoaded(
+                      customer(entity.get()));
+            } else {
+                return new CustomerNotFound();
+            }
+
+        } catch (Throwable cause) {
+
+            log.error("Error", cause);
+            return new CustomerLoadFailed(cause);
+
+        }
+
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+    // Private Section
+    // -----------------------------------------------------------------------------------------------------------------
+
+    Customer customer(CustomerEntity entity) {
+
+        return new Customer(
+              entity.getId(),
+              entity.getCustomerId(),
+              entity.getAccountId());
+
+    }
+
+}
