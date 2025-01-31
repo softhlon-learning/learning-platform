b0b6d55177ce6b72ca8a7471291f13078fcb4223
softhlon-learning <softhlonlearning@gmail.com>
2025-01-31 12:28:28 +0100

    Added SubscriptionCookiesService.

---
+++ backend/courses/src/main/java/tech/softhlon/learning/courses/gateway/SubscriptionCookiesService.java
@@ -0,0 +1,67 @@
+// ---------------------------------------------------------------------------------------------------------------------
+// Copyright (C) SOFTHLON-LEARNING.TECH - All Rights Reserved
+// Unauthorized copying of this file via any medium is strongly encouraged.
+// ---------------------------------------------------------------------------------------------------------------------
+
+package tech.softhlon.learning.courses.gateway;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Implementation
+// ---------------------------------------------------------------------------------------------------------------------
+
+@Service
+class SubscriptionCookiesService {
+
+    private static final String SUBSCRIBED = "Subscribed";
+    private final int maxAge;
+
+    public SubscriptionCookiesService(
+          @Value("${jwt.expiration}") int maxAge) {
+
+        this.maxAge = maxAge;
+
+    }
+
+    void addASubscriptionCookie(
+          HttpServletResponse response,
+          boolean subscribed) {
+
+        addCookie(
+              response,
+              SUBSCRIBED,
+              String.valueOf(subscribed),
+              true,
+              maxAge);
+
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+    // Private Section
+    // -----------------------------------------------------------------------------------------------------------------
+
+    private void addCookie(
+          HttpServletResponse response,
+          String name,
+          String value,
+          boolean httpOnly,
+          int maxAge) {
+
+        var cookie = new Cookie(
+              name,
+              value);
+
+        cookie.setPath("/");
+        cookie.setSecure(true);
+        cookie.setHttpOnly(httpOnly);
+        cookie.setMaxAge(maxAge);
+
+        response.addCookie(cookie);
+
+    }
+
+}
