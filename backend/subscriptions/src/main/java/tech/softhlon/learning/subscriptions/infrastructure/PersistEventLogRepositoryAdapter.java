ec436c0a59f0540849291f7ac21e1302d4b84588
softhlon-learning <softhlonlearning@gmail.com>
2025-01-31 16:50:04 +0100

    subscriptions: Implement PersistEventLogRepositoryAdapter

--- backend/subscriptions/src/main/java/tech/softhlon/learning/subscriptions/infrastructure/PersistEventLogRepositoryAdapter.java
+++ backend/subscriptions/src/main/java/tech/softhlon/learning/subscriptions/infrastructure/PersistEventLogRepositoryAdapter.java
@@ -5,20 +5,51 @@
 
 package tech.softhlon.learning.subscriptions.infrastructure;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import tech.softhlon.learning.subscriptions.domain.PersistEventLogRepository;
+import tech.softhlon.learning.subscriptions.domain.PersistEventLogRepository.PersistEventLogResult.EventLogPersisted;
+import tech.softhlon.learning.subscriptions.domain.PersistEventLogRepository.PersistEventLogResult.EventLogPersistenceFailed;
 
 // ---------------------------------------------------------------------------------------------------------------------
 // Implementation
 // ---------------------------------------------------------------------------------------------------------------------
 
+@Slf4j
 @Service
+@RequiredArgsConstructor
 class PersistEventLogRepositoryAdapter implements PersistEventLogRepository {
+    private final EventLogJpaRepository eventLogJpaRepository;
 
     @Override
     public PersistEventLogResult execute(
           PersistEventLogRequest request) {
-        return null;
+
+        try {
+
+            eventLogJpaRepository.save(entity(request));
+            return new EventLogPersisted();
+
+        } catch (Throwable cause) {
+
+            log.error("Error", cause);
+            return new EventLogPersistenceFailed(cause)
+
+        }
     }
 
+    // -----------------------------------------------------------------------------------------------------------------
+    // Private Section
+    // -----------------------------------------------------------------------------------------------------------------
+
+    private EventLogEntity entity(
+          PersistEventLogRequest request) {
+
+        return EventLogEntity.builder()
+              .customerId(request.customerId())
+              .payload(request.payload())
+              .build();
+
+    }
 }
