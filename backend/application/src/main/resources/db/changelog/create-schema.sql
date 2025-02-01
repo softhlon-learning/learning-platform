9d3300e1717972e128259df80a1f370bdf69437f
softhlon-learning <softhlonlearning@gmail.com>
2025-02-01 15:37:23 +0100

    database: Rename all created_time and updated_time columns to created_at and updated_at

--- backend/application/src/main/resources/db/changelog/create-schema.sql
+++ backend/application/src/main/resources/db/changelog/create-schema.sql
@@ -6,7 +6,7 @@ CREATE TABLE accounts (
     password VARCHAR NULL,
     is_deleted BOOLEAN NOT NULL DEFAULT false,
     created_time TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
-    updated_time TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
     PRIMARY KEY (id),
     CONSTRAINT unique_accounts_email UNIQUE (email)
 );
@@ -19,8 +19,8 @@ CREATE TABLE courses (
     description VARCHAR,
     content VARCHAR,
     version VARCHAR NOT NULL,
-    created_time TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
-    updated_time TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
     PRIMARY KEY (id)
 );
 
@@ -31,8 +31,8 @@ CREATE TABLE enrollments (
     content VARCHAR,
     enrolled_time TIMESTAMP WITH TIME ZONE,
     completed_time TIMESTAMP WITH TIME ZONE,
-    created_time TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
-    updated_time TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
     PRIMARY KEY (id),
     UNIQUE(account_id, course_id),
     CONSTRAINT fk_enrollments_accounts
@@ -47,7 +47,7 @@ CREATE TABLE customers (
     id uuid DEFAULT gen_random_uuid(),
     account_id uuid NOT NULL,
     customer_id VARCHAR NOT NULL,
-    created_time TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
     PRIMARY KEY (id),
         CONSTRAINT fk_customers_accounts
              FOREIGN KEY (account_id)
@@ -65,15 +65,15 @@ CREATE TABLE subscriptions (
     canceled_at TIMESTAMP WITH TIME ZONE,
     cancel_at TIMESTAMP WITH TIME ZONE,
     cancel_reason VARCHAR NOT NULL,
-    created_time TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
-    updated_time TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE invalidated_tokens (
     id uuid DEFAULT gen_random_uuid(),
     token_hash VARCHAR NOT NULL,
-    created_time TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
     PRIMARY KEY (id)
 );
 
@@ -84,7 +84,7 @@ CREATE TABLE reset_password_tokens (
     account_id uuid,
     token VARCHAR NOT NULL,
     expiration_time TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
-    created_time TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
     PRIMARY KEY (id),
     CONSTRAINT fk_reset_password_tokens_accounts
          FOREIGN KEY (account_id)
@@ -99,8 +99,8 @@ CREATE TABLE checkout_sessions (
     session_id VARCHAR NOT NULL,
     expired_time TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
     completed_time TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
-    created_time TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
-    updated_time TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
     PRIMARY KEY (id),
     CONSTRAINT fk_checkout_sessions_accounts
          FOREIGN KEY (account_id)
@@ -114,7 +114,7 @@ CREATE TABLE events_log (
     event_type VARCHAR NOT NULL,
     customer_id VARCHAR NOT NULL,
     payload VARCHAR NOT NULL,
-    created_time TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
     PRIMARY KEY (id)
 );
 
@@ -122,7 +122,7 @@ CREATE OR REPLACE FUNCTION update_modified_column()
 RETURNS TRIGGER AS $$
 BEGIN
    IF row(NEW.*) IS DISTINCT FROM row(OLD.*) THEN
-      NEW.updated_time = now();
+      NEW.updated_at = now();
       RETURN NEW;
    ELSE
       RETURN OLD;
@@ -130,8 +130,8 @@ BEGIN
 END;
 $$ language 'plpgsql';
 
-CREATE TRIGGER update_accounts_updated_time BEFORE UPDATE ON accounts FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
-CREATE TRIGGER update_courses_updated_time BEFORE UPDATE ON courses FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
-CREATE TRIGGER update_enrollments_updated_time BEFORE UPDATE ON enrollments FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
-CREATE TRIGGER update_subscriptions_updated_time BEFORE UPDATE ON subscriptions FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
-CREATE TRIGGER update_checkout_sessions_updated_time BEFORE UPDATE ON checkout_sessions FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
+CREATE TRIGGER update_accounts_updated_at BEFORE UPDATE ON accounts FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
+CREATE TRIGGER update_courses_updated_at BEFORE UPDATE ON courses FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
+CREATE TRIGGER update_enrollments_updated_at BEFORE UPDATE ON enrollments FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
+CREATE TRIGGER update_subscriptions_updated_at BEFORE UPDATE ON subscriptions FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
+CREATE TRIGGER update_checkout_sessions_updated_at BEFORE UPDATE ON checkout_sessions FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
