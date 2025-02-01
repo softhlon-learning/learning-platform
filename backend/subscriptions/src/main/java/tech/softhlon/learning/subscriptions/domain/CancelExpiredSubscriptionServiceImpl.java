fa2a604c2d358efde54b5965659108a2322456a3
softhlon-learning <softhlonlearning@gmail.com>
2025-02-01 23:41:02 +0100

    subscriptions: Set MDC in scheduler

--- backend/subscriptions/src/main/java/tech/softhlon/learning/subscriptions/domain/CancelExpiredSubscriptionServiceImpl.java
+++ backend/subscriptions/src/main/java/tech/softhlon/learning/subscriptions/domain/CancelExpiredSubscriptionServiceImpl.java
@@ -7,6 +7,7 @@ package tech.softhlon.learning.subscriptions.domain;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -21,9 +22,13 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 class CancelExpiredSubscriptionServiceImpl implements CancelExpiredSubscriptionService {
 
+    private static final String ACCOUNT = "account";
+    private static final String SCHEDULER = "scheduler";
+
     @Override
-    @Scheduled(fixedRate = 3600_000)
+    @Scheduled(fixedRate = 1800_000)
     public void execute() {
+        MDC.put(ACCOUNT, SCHEDULER);
         log.info("service | Run cancel expired subscriptions");
     }
 
