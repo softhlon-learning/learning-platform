adfcbb3b9a6ed8d042a20273a085f5c4fe20ddd7
softhlon-learning <softhlonlearning@gmail.com>
2025-02-02 15:32:06 +0100

    subscriptions: Add PersistInvoiceRepository

---
+++ backend/subscriptions/src/main/java/tech/softhlon/learning/subscriptions/domain/PersistInvoiceRepository.java
@@ -0,0 +1,30 @@
+// ---------------------------------------------------------------------------------------------------------------------
+// Copyright (C) SOFTHLON-LEARNING.TECH - All Rights Reserved
+// Unauthorized copying of this file via any medium is strictly prohibited.
+// ---------------------------------------------------------------------------------------------------------------------
+
+package tech.softhlon.learning.subscriptions.domain;
+
+import tech.softhlon.learning.common.hexagonal.OutboundPort;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Implementation
+// ---------------------------------------------------------------------------------------------------------------------
+
+@OutboundPort
+@FunctionalInterface
+public interface PersistInvoiceRepository {
+
+    PersistInvoiceResult execute(
+          PersistInvoiceRequest request);
+
+    sealed interface PersistInvoiceResult {
+        record CheckoutSessionPersisted() implements PersistInvoiceResult {}
+        record CheckoutSessionPersistenceFailed(Throwable cause) implements PersistInvoiceResult {}
+    }
+
+    record PersistInvoiceRequest(
+          String invoiceId,
+          boolean paid) {}
+
+}
