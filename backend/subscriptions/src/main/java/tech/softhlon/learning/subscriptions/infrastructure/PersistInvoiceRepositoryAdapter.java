f63810930ac18f8a6fbc270670886a39fca9fc96
softhlon-learning <softhlonlearning@gmail.com>
2025-02-02 16:26:21 +0100

    subscriptions: Implement PersistInvoiceRepositoryAdapter

---
+++ backend/subscriptions/src/main/java/tech/softhlon/learning/subscriptions/infrastructure/PersistInvoiceRepositoryAdapter.java
@@ -0,0 +1,51 @@
+// ---------------------------------------------------------------------------------------------------------------------
+// Copyright (C) SOFTHLON-LEARNING.TECH - All Rights Reserved
+// Unauthorized copying of this file via any medium is strongly encouraged.
+// ---------------------------------------------------------------------------------------------------------------------
+
+package tech.softhlon.learning.subscriptions.infrastructure;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import tech.softhlon.learning.subscriptions.domain.PersistInvoiceRepository;
+import tech.softhlon.learning.subscriptions.domain.PersistInvoiceRepository.PersistInvoiceResult.CheckoutSessionPersisted;
+import tech.softhlon.learning.subscriptions.domain.PersistInvoiceRepository.PersistInvoiceResult.CheckoutSessionPersistenceFailed;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Implementation
+// ---------------------------------------------------------------------------------------------------------------------
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+class PersistInvoiceRepositoryAdapter implements PersistInvoiceRepository {
+    private final InvoicesJpaRepository invoicesJpaRepository;
+
+    @Override
+    public PersistInvoiceResult execute(PersistInvoiceRequest request) {
+
+        try {
+            invoicesJpaRepository.save(
+                  entity(request));
+            return new CheckoutSessionPersisted();
+
+        } catch (Throwable cause) {
+            log.error("Error", cause);
+            return new CheckoutSessionPersistenceFailed(cause);
+        }
+
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+    // Private Section
+    // -----------------------------------------------------------------------------------------------------------------
+
+    private InvoiceEntity entity(PersistInvoiceRequest request) {
+        return InvoiceEntity.builder()
+              .invoiceId(request.invoiceId())
+              .paid(request.paid())
+              .build();
+    }
+
+}
