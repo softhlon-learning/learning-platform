0be6cb4d3db52fca466ddb9ac467e1570265de14
softhlon-learning <softhlonlearning@gmail.com>
2025-02-02 01:26:00 +0100

    database: Add schema per module and group tables accordingly

--- backend/application/src/main/resources/db/changelog/create-schema.sql
+++ backend/application/src/main/resources/db/changelog/create-schema.sql
@@ -1,6 +1,8 @@
--- accounts module
+-- _accounts schema
 
-CREATE TABLE accounts (
+CREATE SCHEMA _accounts;
+
+CREATE TABLE _accounts.accounts (
     id uuid DEFAULT gen_random_uuid(),
     name VARCHAR NOT NULL,
     type VARCHAR NOT NULL,
@@ -13,16 +15,16 @@ CREATE TABLE accounts (
     UNIQUE (email)
 );
 
-CREATE TABLE invalidated_tokens (
+CREATE TABLE _accounts.invalidated_tokens (
     id uuid DEFAULT gen_random_uuid(),
     token_hash VARCHAR NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
     PRIMARY KEY (id)
 );
 
-CREATE INDEX invalidated_tokens__token_hash_index ON invalidated_tokens (token_hash);
+CREATE INDEX invalidated_tokens__token_hash_index ON _accounts.invalidated_tokens (token_hash);
 
-CREATE TABLE reset_password_tokens (
+CREATE TABLE _accounts.reset_password_tokens (
     id uuid DEFAULT gen_random_uuid(),
     account_id uuid,
     token VARCHAR NOT NULL,
@@ -31,14 +33,16 @@ CREATE TABLE reset_password_tokens (
     PRIMARY KEY (id),
     CONSTRAINT fk_reset_password_tokens_accounts
          FOREIGN KEY (account_id)
-         REFERENCES accounts (id)
+         REFERENCES _accounts.accounts (id)
 );
 
-CREATE INDEX reset_password_tokens__token_index ON reset_password_tokens (token);
+CREATE INDEX reset_password_tokens__token_index ON _accounts.reset_password_tokens (token);
 
--- courses module
+-- _courses schema
 
-CREATE TABLE courses (
+CREATE SCHEMA _courses;
+
+CREATE TABLE _courses.courses (
     id uuid,
     code VARCHAR NOT NULL,
     order_no INTEGER,
@@ -51,7 +55,7 @@ CREATE TABLE courses (
     PRIMARY KEY (id)
 );
 
-CREATE TABLE enrollments (
+CREATE TABLE _courses.enrollments (
     id uuid DEFAULT gen_random_uuid(),
     account_id uuid NOT NULL,
     course_id uuid NOT NULL,
@@ -61,18 +65,20 @@ CREATE TABLE enrollments (
     created_at TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
     PRIMARY KEY (id),
-    UNIQUE(account_id, course_id),
+    UNIQUE (account_id, course_id),
     CONSTRAINT fk_enrollments_accounts
          FOREIGN KEY (account_id)
-         REFERENCES accounts (id),
+         REFERENCES _accounts.accounts (id),
     CONSTRAINT fk_enrollments_courses
          FOREIGN KEY (course_id)
-         REFERENCES courses (id)
+         REFERENCES _courses.courses (id)
 );
 
--- subscriptions
+-- _subscriptions schema
 
-CREATE TABLE customers (
+CREATE SCHEMA _subscriptions;
+
+CREATE TABLE _subscriptions.customers (
     id uuid DEFAULT gen_random_uuid(),
     account_id uuid NOT NULL,
     customer_id VARCHAR NOT NULL,
@@ -80,14 +86,14 @@ CREATE TABLE customers (
     PRIMARY KEY (id),
         CONSTRAINT fk_customers_accounts
              FOREIGN KEY (account_id)
-             REFERENCES accounts (id),
+             REFERENCES _accounts.accounts (id),
     UNIQUE (account_id),
     UNIQUE (customer_id)
 );
 
-CREATE INDEX customers__customer_id_index ON customers (customer_id);
+CREATE INDEX customers__customer_id_index ON _subscriptions.customers (customer_id);
 
-CREATE TABLE subscriptions (
+CREATE TABLE _subscriptions.subscriptions (
     id uuid DEFAULT gen_random_uuid(),
     subscription_id VARCHAR NOT NULL,
     customer_id VARCHAR NOT NULL,
@@ -103,7 +109,7 @@ CREATE TABLE subscriptions (
     PRIMARY KEY (id)
 );
 
-CREATE TABLE checkout_sessions (
+CREATE TABLE _subscriptions.checkout_sessions (
     id uuid DEFAULT gen_random_uuid(),
     account_id uuid,
     session_id VARCHAR NOT NULL,
@@ -114,12 +120,12 @@ CREATE TABLE checkout_sessions (
     PRIMARY KEY (id),
     CONSTRAINT fk_checkout_sessions_accounts
          FOREIGN KEY (account_id)
-         REFERENCES accounts (id)
+         REFERENCES _accounts.accounts (id)
 );
 
-CREATE INDEX checkout_sessions__session_id_index ON checkout_sessions (session_id);
+CREATE INDEX checkout_sessions__session_id_index ON _subscriptions.checkout_sessions (session_id);
 
-CREATE TABLE events_log (
+CREATE TABLE _subscriptions.events_log (
     id uuid DEFAULT gen_random_uuid(),
     event_type VARCHAR NOT NULL,
     customer_id VARCHAR NOT NULL,
@@ -128,7 +134,7 @@ CREATE TABLE events_log (
     PRIMARY KEY (id)
 );
 
-CREATE TABLE invoices (
+CREATE TABLE _subscriptions.invoices (
     id uuid DEFAULT gen_random_uuid(),
     invoice_id VARCHAR NOT NULL,
     paid BOOLEAN DEFAULT false,
@@ -138,7 +144,7 @@ CREATE TABLE invoices (
     UNIQUE (invoice_id)
 );
 
--- common
+---- common
 
 CREATE OR REPLACE FUNCTION update_modified_column()
 RETURNS TRIGGER AS $$
@@ -152,8 +158,8 @@ BEGIN
 END;
 $$ language 'plpgsql';
 
-CREATE TRIGGER update_accounts_updated_at BEFORE UPDATE ON accounts FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
-CREATE TRIGGER update_courses_updated_at BEFORE UPDATE ON courses FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
-CREATE TRIGGER update_enrollments_updated_at BEFORE UPDATE ON enrollments FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
-CREATE TRIGGER update_subscriptions_updated_at BEFORE UPDATE ON subscriptions FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
-CREATE TRIGGER update_checkout_sessions_updated_at BEFORE UPDATE ON checkout_sessions FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
+CREATE TRIGGER update_accounts_updated_at BEFORE UPDATE ON _accounts.accounts FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
+CREATE TRIGGER update_courses_updated_at BEFORE UPDATE ON _courses.courses FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
+CREATE TRIGGER update_enrollments_updated_at BEFORE UPDATE ON _courses.enrollments FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
+CREATE TRIGGER update_subscriptions_updated_at BEFORE UPDATE ON _subscriptions.subscriptions FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
+CREATE TRIGGER update_checkout_sessions_updated_at BEFORE UPDATE ON _subscriptions.checkout_sessions FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
