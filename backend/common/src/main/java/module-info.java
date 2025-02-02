b875288b50eb9d00cbc682faae4602030bff8d63
softhlon-learning <softhlonlearning@gmail.com>
2025-02-02 23:02:36 +0100

    common: Sort/clean up module-info.java

--- backend/common/src/main/java/module-info.java
+++ backend/common/src/main/java/module-info.java
@@ -1,16 +1,17 @@
 module common {
-    requires static lombok;
-    requires spring.context;
-    requires spring.web;
-    requires org.apache.tomcat.embed.core;
-    requires org.slf4j;
-    requires spring.security.core;
-    requires spring.beans;
-
     exports tech.softhlon.learning.common.domain;
     exports tech.softhlon.learning.common.event;
     exports tech.softhlon.learning.common.hexagonal;
     exports tech.softhlon.learning.common.controller;
     exports tech.softhlon.learning.common.security;
     exports tech.softhlon.learning.common.text;
+
+    requires static lombok;
+
+    requires spring.context;
+    requires spring.web;
+    requires spring.security.core;
+    requires spring.beans;
+    requires org.apache.tomcat.embed.core;
+    requires org.slf4j;
 }
