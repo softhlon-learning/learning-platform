5f5b470e38e2d47ee2ee76949ac90a476ac6e9a8
softhlon-learning <softhlonlearning@gmail.com>
2025-02-02 23:01:54 +0100

    accounts: Sort/clean up module-info.java

--- backend/accounts/src/main/java/module-info.java
+++ backend/accounts/src/main/java/module-info.java
@@ -1,12 +1,11 @@
 module accounts {
     exports tech.softhlon.learning.accounts.gateway;
     requires common;
+
     requires static lombok;
+
     requires spring.context;
     requires spring.web;
-    requires org.apache.tomcat.embed.core;
-    requires org.slf4j;
-    requires jakarta.persistence;
     requires spring.data.jpa;
     requires spring.data.commons;
     requires spring.boot.autoconfigure;
@@ -14,15 +13,19 @@ module accounts {
     requires spring.core;
     requires spring.webmvc;
     requires spring.beans;
-    requires google.api.client;
-    requires com.google.api.client;
-    requires com.google.api.client.json.gson;
-    requires com.google.api.client.auth;
-    requires java.xml.bind;
-    requires jjwt.api;
-    requires com.fasterxml.jackson.annotation;
     requires spring.security.core;
     requires spring.security.web;
     requires spring.security.config;
     requires spring.context.support;
+
+    requires java.xml.bind;
+    requires jjwt.api;
+    requires jakarta.persistence;
+    requires com.fasterxml.jackson.annotation;
+    requires org.apache.tomcat.embed.core;
+    requires org.slf4j;
+    requires google.api.client;
+    requires com.google.api.client;
+    requires com.google.api.client.json.gson;
+    requires com.google.api.client.auth;
 }
