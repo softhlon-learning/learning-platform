c729351bd1e29aab555eaf27d550a13dcdd0b855
softhlon-learning <softhlonlearning@gmail.com>
2025-02-03 13:39:53 +0100

    frontend: Add constants.ts

---
+++ frontend/src/app/common/constants.ts
@@ -0,0 +1,4 @@
+export const AUTHENTICATED_COOKIE = "Authenticated";
+export const SUBSCRIBED_COOKIE = "Subscribed";
+export const GO_BACK_APTH_COOKIE = "GoBackPath";
+export const REDIRECT_COOKIE = "Redirect";
