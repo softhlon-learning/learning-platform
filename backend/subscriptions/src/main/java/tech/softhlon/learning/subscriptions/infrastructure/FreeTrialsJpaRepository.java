a2015616bc462dcadb6a4dae124dffaf91e3a011
softhlon-learning <softhlonlearning@gmail.com>
2025-02-04 22:17:57 +0100

    subscriptions: Create FreeTrialsJpaRepository and entity

---
+++ backend/subscriptions/src/main/java/tech/softhlon/learning/subscriptions/infrastructure/FreeTrialsJpaRepository.java
@@ -0,0 +1,21 @@
+// ---------------------------------------------------------------------------------------------------------------------
+// Copyright (C) SOFTHLON-LEARNING.TECH - All Rights Reserved
+// Unauthorized copying of this file via any medium is strictly prohibited.
+// ---------------------------------------------------------------------------------------------------------------------
+
+package tech.softhlon.learning.subscriptions.infrastructure;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Implementation
+// ---------------------------------------------------------------------------------------------------------------------
+
+/**
+ * Free tials JPA repository interface.
+ */
+interface FreeTrialsJpaRepository extends CrudRepository<FreeTrialEntity, UUID> {
+}
