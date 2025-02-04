6f5625709f18ae0b8b267efa6700b314ac5924a7
softhlon-learning <softhlonlearning@gmail.com>
2025-02-04 22:42:56 +0100

    subscriptions: Add LoadFreeTrialRepositoryAdapter

---
+++ backend/subscriptions/src/main/java/tech/softhlon/learning/subscriptions/infrastructure/LoadFreeTrialRepositoryAdapter.java
@@ -0,0 +1,67 @@
+// ---------------------------------------------------------------------------------------------------------------------
+// Copyright (C) SOFTHLON-LEARNING.TECH - All Rights Reserved
+// Unauthorized copying of this file via any medium is strictly prohibited.
+// ---------------------------------------------------------------------------------------------------------------------
+
+package tech.softhlon.learning.subscriptions.infrastructure;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import tech.softhlon.learning.subscriptions.domain.LoadFreeTrialRepository;
+import tech.softhlon.learning.subscriptions.domain.LoadFreeTrialRepository.LoadFreeTrialResult.FreeTrialLoadFailed;
+import tech.softhlon.learning.subscriptions.domain.LoadFreeTrialRepository.LoadFreeTrialResult.FreeTrialLoaded;
+import tech.softhlon.learning.subscriptions.domain.LoadFreeTrialRepository.LoadFreeTrialResult.FreeTrialNotFound;
+
+import java.util.UUID;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Implementation
+// ---------------------------------------------------------------------------------------------------------------------
+
+/**
+ * Load free trial repository adapter implementation.
+ */
+@Slf4j
+@Service
+@RequiredArgsConstructor
+class LoadFreeTrialRepositoryAdapter implements LoadFreeTrialRepository {
+    private final FreeTrialsJpaRepository freeTrialsJpaRepository;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public LoadFreeTrialResult execute(
+          UUID accountId) {
+
+        try {
+            var entity = freeTrialsJpaRepository.findById(accountId);
+
+            if (entity.isPresent()) {
+                return new FreeTrialLoaded(customer(entity.get()));
+            } else {
+                return new FreeTrialNotFound();
+            }
+        } catch (Throwable cause) {
+            log.error("Error", cause);
+            return new FreeTrialLoadFailed(cause);
+        }
+
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+    // Private Section
+    // -----------------------------------------------------------------------------------------------------------------
+
+    private FreeTrial customer(FreeTrialEntity entity) {
+
+        return new FreeTrial(
+              entity.getId(),
+              entity.getAccountId(),
+              entity.getExpireAt()
+        );
+
+    }
+
+}
