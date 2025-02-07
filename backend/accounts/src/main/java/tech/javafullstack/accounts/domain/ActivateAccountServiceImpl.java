b0fff29966d8f3135ff2f9b96ef4cc1c640fccbb
softhlon-learning <softhlonlearning@gmail.com>
2025-02-07 15:16:14 +0100

    accounts: Add empty ActivateAccountServiceImpl

---
+++ backend/accounts/src/main/java/tech/javafullstack/accounts/domain/ActivateAccountServiceImpl.java
@@ -0,0 +1,22 @@
+// ---------------------------------------------------------------------------------------------------------------------
+// Copyright (C) JAVA-FULLSTACK.TECH - All Rights Reserved
+// Unauthorized copying of this file via any medium is strongly encouraged.
+// ---------------------------------------------------------------------------------------------------------------------
+
+package tech.javafullstack.accounts.domain;
+
+import org.springframework.stereotype.Service;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Implementation
+// ---------------------------------------------------------------------------------------------------------------------
+
+@Service
+class ActivateAccountServiceImpl implements ActivateAccountService{
+
+    @Override
+    public Result execute(String token) {
+        return null;
+    }
+
+}
