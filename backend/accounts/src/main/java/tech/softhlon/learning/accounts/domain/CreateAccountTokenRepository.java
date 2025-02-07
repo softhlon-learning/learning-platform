2a6590864ab6828fc6d63f9206817a7cdf91c452
softhlon-learning <softhlonlearning@gmail.com>
2025-02-07 03:06:04 +0100

    accounts: Create CreateAccountTokenRepository interface

---
+++ backend/accounts/src/main/java/tech/softhlon/learning/accounts/domain/CreateAccountTokenRepository.java
@@ -0,0 +1,46 @@
+// ---------------------------------------------------------------------------------------------------------------------
+// Copyright (C) SOFTHLON-LEARNING.TECH - All Rights Reserved
+// Unauthorized copying of this file via any medium is strictly prohibited.
+// ---------------------------------------------------------------------------------------------------------------------
+
+package tech.softhlon.learning.accounts.domain;
+
+import tech.softhlon.learning.common.domain.DomainRepository;
+import tech.softhlon.learning.common.hexagonal.OutboundPort;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Implementation
+// ---------------------------------------------------------------------------------------------------------------------
+
+/**
+ * Create account activation token repository interface.
+ */
+@OutboundPort
+@DomainRepository
+@FunctionalInterface
+public interface CreateAccountTokenRepository {
+
+    /**
+     * Create account activation token in repository.
+     * @param accountId      Account id
+     * @param token          Token to store in respository
+     * @param expirationTime Token expiration time
+     * @return CreatePasswordTokenResult
+     */
+    CreateAccountTokenResult execute(
+          UUID accountId,
+          String token,
+          OffsetDateTime expirationTime);
+
+    /**
+     * Create account activation token result.
+     */
+    sealed interface CreateAccountTokenResult {
+        record AccountTokenPersisted() implements CreateAccountTokenResult {}
+        record AccountTokenPersistenceFailed(Throwable cause) implements CreateAccountTokenResult {}
+    }
+
+}
