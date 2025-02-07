9ef045108db020f5504a370e9eefa20550c2c8e6
softhlon-learning <softhlonlearning@gmail.com>
2025-02-07 21:13:29 +0100

    accounts: Implement DeleteAccountTokenRepositoryAdapter

---
+++ backend/accounts/src/main/java/tech/javafullstack/accounts/infrastructure/DeleteAccountTokenRepositoryAdapter.java
@@ -0,0 +1,48 @@
+// ---------------------------------------------------------------------------------------------------------------------
+// Copyright (C) JAVA-FULLSTACK.TECH - All Rights Reserved
+// Unauthorized copying of this file via any medium is strictly prohibited.
+// ---------------------------------------------------------------------------------------------------------------------
+
+package tech.javafullstack.accounts.infrastructure;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import tech.javafullstack.accounts.domain.DeletePasswordTokenRepository;
+import tech.javafullstack.accounts.domain.DeletePasswordTokenRepository.DeletePasswordTokenResult.TokenDeleted;
+import tech.javafullstack.accounts.domain.DeletePasswordTokenRepository.DeletePasswordTokenResult.TokenDeletionFailed;
+
+import java.util.UUID;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Implementation
+// ---------------------------------------------------------------------------------------------------------------------
+
+/**
+ * Delete account activation token repository adapter implementation.
+ */
+@Slf4j
+@Service
+@RequiredArgsConstructor
+class DeleteAccountTokenRepositoryAdapter implements DeletePasswordTokenRepository {
+
+    private final AccountTokensJpaRepository accountTokensJpaRepository;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public DeletePasswordTokenResult execute(
+          UUID id) {
+
+        try {
+            accountTokensJpaRepository.deleteById(id);
+            return new TokenDeleted();
+        } catch (Throwable cause) {
+            log.error("Error", cause);
+            return new TokenDeletionFailed(cause);
+        }
+
+    }
+
+}
