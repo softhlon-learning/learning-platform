89e04d865446b3a38bcfa089b4dc56947eb0f5a6
softhlon-learning <softhlonlearning@gmail.com>
2025-02-07 15:10:53 +0100

    accounts: Add ActivateAccountService

---
+++ backend/accounts/src/main/java/tech/javafullstack/accounts/domain/ActivateAccountService.java
@@ -0,0 +1,39 @@
+// ---------------------------------------------------------------------------------------------------------------------
+// Copyright (C) JAVA-FULLSTACK.TECH - All Rights Reserved
+// Unauthorized copying of this file via any medium is strictly prohibited.
+// ---------------------------------------------------------------------------------------------------------------------
+
+package tech.javafullstack.accounts.domain;
+
+import tech.javafullstack.common.hexagonal.InboundPort;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Implementation
+// ---------------------------------------------------------------------------------------------------------------------
+
+/**
+ * Activate account service interface.
+ */
+@InboundPort
+@FunctionalInterface
+public interface ActivateAccountService {
+
+    /**
+     * Activate account.
+     * @param token Account activation token
+     * @return Result
+     */
+    Result execute(
+          String token);
+
+    /**
+     * Activate account result.
+     */
+    sealed interface Result {
+        record Succeeded() implements Result {}
+        record InvalidTokenFailed(String message) implements Result {}
+        record ExpiredTokenFailed(String message) implements Result {}
+        record Failed(Throwable cause) implements Result {}
+    }
+
+}
