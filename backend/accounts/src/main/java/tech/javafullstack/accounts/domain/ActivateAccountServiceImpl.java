b4409cf44b2958729989cd858f984e9b8147a03a
softhlon-learning <softhlonlearning@gmail.com>
2025-02-07 15:54:04 +0100

    accounts: Implement partially ActivateAccountServiceImpl

--- backend/accounts/src/main/java/tech/javafullstack/accounts/domain/ActivateAccountServiceImpl.java
+++ backend/accounts/src/main/java/tech/javafullstack/accounts/domain/ActivateAccountServiceImpl.java
@@ -5,18 +5,39 @@
 
 package tech.javafullstack.accounts.domain;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import tech.javafullstack.accounts.domain.ActivateAccountService.Result.Failed;
+import tech.javafullstack.accounts.domain.ActivateAccountService.Result.InvalidTokenFailed;
+import tech.javafullstack.accounts.domain.ActivateAccountService.Result.Succeeded;
+import tech.javafullstack.accounts.domain.LoadAccountTokenRepository.LoadAccountTokenResult.TokenLoadFailed;
+import tech.javafullstack.accounts.domain.LoadAccountTokenRepository.LoadAccountTokenResult.TokenLoaded;
+import tech.javafullstack.accounts.domain.LoadAccountTokenRepository.LoadAccountTokenResult.TokenNotFound;
 
 // ---------------------------------------------------------------------------------------------------------------------
 // Implementation
 // ---------------------------------------------------------------------------------------------------------------------
 
 @Service
-class ActivateAccountServiceImpl implements ActivateAccountService{
+@RequiredArgsConstructor
+class ActivateAccountServiceImpl implements ActivateAccountService {
+
+    private static final String INVALID_TOKEN = "Invalid account activation token";
+    private static final String EXPIRED_TOKEN = "Account activation token has expired";
+
+    private final LoadAccountTokenRepository loadAccountTokenRepository;
 
     @Override
     public Result execute(String token) {
-        return null;
+
+        var result = loadAccountTokenRepository.execute(token);
+
+        return switch (result) {
+            case TokenLoaded tokenLoaded -> new Succeeded();
+            case TokenNotFound() -> new InvalidTokenFailed("");
+            case TokenLoadFailed(Throwable cause) -> new Failed(cause);
+        };
+
     }
 
 }
