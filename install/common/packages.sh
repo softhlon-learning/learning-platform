1b03aa714f9c255c4d623135388b6d9d0ecfeb3f
softhlon-learning <softhlonlearning@gmail.com>
2025-02-12 19:38:05 +0100

    system: Create packages.sh

--- install/common/packages.sh
+++ install/common/packages.sh
@@ -1,1 +1,27 @@
 #!/bin/sh
+
+# system packages
+pkg install -y vim
+pkg install -y htop
+pkg install -y sudo
+pkg install -y zsh
+pkg install -y ohmyzsh
+pkg install -y maven
+pkg install -y openjdk23
+pkg install -y nginx
+pkg install -y node22
+pkg install -y npm-node22
+pkg install -y postfix
+pkg install -y postgresql17-server
+
+# npm packages
+npm install -y -g @angular/cli
+npm install -y -g yarn
+
+# rc.conf configuration
+sudo sysrc pf_enable=yes
+sudo sysrc nginx_enable=yes
+sudo sysrc postfix_enable=yes
+sudo sysrc postgresql_enable=yes
+sudo sysrc backend_enable=yes
+sudo sysrc frontend_enable=yes
