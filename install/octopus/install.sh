122b7f207d69e2ac03b0c454332a1a6a3edfd748
softhlon-learning <softhlonlearning@gmail.com>
2025-02-12 19:55:51 +0100

    system: Update path in octopus/install.sh -

--- install/octopus/install.sh
+++ install/octopus/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 echo "Install <octopus> server start."
-chmod +x root/platform/install/common/packages.sh
-root/platform/install/common/packages.sh
+chmod +x /root/platform/install/common/packages.sh
+/root/platform/install/common/packages.sh
 echo "Install <octopus> server stop."
