a6ef89d47e04afc75db5174ae0bed57ee0d55bc4
softhlon-learning <softhlonlearning@gmail.com>
2025-02-13 21:42:29 +0100

    system: Update make_slave.sh

--- config/scripts/make_slave.sh
+++ config/scripts/make_slave.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-sudo -u postgres pg_dump learning > /tmp/learning.dump
+# dump slave
+sudo -u postgres pg_dump -c learning > /tmp/learning.dump
+
+# restore master from slave
+scp /tmp/learning.dump admin@10.0.0.1:
+ssh admin@10.0.0.1 -t 'sudo -u postgres psql -d learning < /home/admin/learning.dump'
+
+# continue on slave
 sudo service postgresql stop
 
 sudo -u postgres cp /var/db/postgres/data17/postgresql.conf /tmp/postgresql.conf
