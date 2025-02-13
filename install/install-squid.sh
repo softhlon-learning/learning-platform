ec0255b649207c6b0ad7086b81a1ff9f417e176c
softhlon-learning <softhlonlearning@gmail.com>
2025-02-13 02:05:51 +0100

    system: Prepare install-squid.sh script

--- install/install-squid.sh
+++ install/install-squid.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
-echo "Install <octopus> server start."
-echo "Install <octopus> server stop."
+echo "Install <squid> server start."
+chmod +x /root/platform/install/common.sh
+
+# common/shared stuff
+/root/platform/install/common.sh
+
+# postfix
+newaliases
+cp /root/platform/config/postfix/main.cf.squid /usr/local/etc/postfix/main.cf
+service postfix start
+
+echo "Install <squid> server stop."
