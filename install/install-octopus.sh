671bad51e0063c5e28a9fff3ce42ff97ee5b1df5
softhlon-learning <softhlonlearning@gmail.com>
2025-02-13 01:11:31 +0100

    system: Improve octopus install script

--- install/install-octopus.sh
+++ install/install-octopus.sh
@@ -2,5 +2,13 @@
 
 echo "Install <octopus> server start."
 chmod +x /root/platform/install/common.sh
+
+# common/shared stuff
 /root/platform/install/common.sh
+
+# postfix
+newaliases
+cp /root/platform/config/postfix/main.cf.octopus /usr/local/etc/postfix/main.cf
+service postgresql start
+
 echo "Install <octopus> server stop."
