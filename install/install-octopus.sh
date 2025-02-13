66911aa27bdce85bed70466c3daac5f8b9c830bb
softhlon-learning <softhlonlearning@gmail.com>
2025-02-13 11:30:20 +0100

    system: Restart postgres after replica setup

--- install/install-octopus.sh
+++ install/install-octopus.sh
@@ -24,5 +24,8 @@ echo "host    replication     replication     10.0.0.2/32   md5" >> /var/db/post
 # postgresql.conf update
 echo "listen_addresses = 'localhost,10.0.0.1'" >> /var/db/postgres/data17/postgresql.conf
 
+# restart service
+service postgresql restart
+
 # stop
 echo "Install octopus server stop."
