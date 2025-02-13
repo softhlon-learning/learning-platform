76741ea9a9c4b7f43730282f61820a4ddc38d6a0
softhlon-learning <softhlonlearning@gmail.com>
2025-02-13 11:05:46 +0100

    system: Reorder newaliases command

--- install/install-octopus.sh
+++ install/install-octopus.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-echo "Install <octopus> server start."
+echo "Install octopus server start."
 chmod +x /root/platform/install/common.sh
 
 # common/shared stuff
 /root/platform/install/common.sh
 
 # postfix
-newaliases
 cp /root/platform/config/postfix/main.cf.octopus /usr/local/etc/postfix/main.cf
 service postfix start
+newaliases
 
-echo "Install <octopus> server stop."
+echo "Install octopus server stop."
--- install/install-squid.sh
+++ install/install-squid.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-echo "Install <squid> server start."
+echo "Install squid server start."
 chmod +x /root/platform/install/common.sh
 
 # common/shared stuff
 /root/platform/install/common.sh
 
 # postfix
-newaliases
 cp /root/platform/config/postfix/main.cf.squid /usr/local/etc/postfix/main.cf
 service postfix start
+newaliases
 
-echo "Install <squid> server stop."
+echo "Install squid server stop."
