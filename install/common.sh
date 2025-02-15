633106ce62ea60816c7d3b6ab404bf3dbab35ad4
softhlon-learning <softhlonlearning@gmail.com>
2025-02-15 13:11:04 +0100

    scripts: Create user report

--- install/common.sh
+++ install/common.sh
@@ -49,6 +49,9 @@ pw adduser platform -d /home/platform -s /bin/sh -c "Learning Platform" -m -w no
 # user support
 pw adduser support -d /home/support -s /bin/sh -c "Support Team" -m -w no
 
+# user report
+pw adduser report -d /home/report -s /bin/sh -c "DMARC Report" -m -w no
+
 # nginx
 mkdir -p /home/admin/nginx
 cp -r /root/platform/config/nginx/nginx.conf /usr/local/etc/nginx/nginx.conf
