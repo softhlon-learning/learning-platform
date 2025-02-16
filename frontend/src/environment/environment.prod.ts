419abe03d7ed88def4a62b4f977279c231cc5144
softhlon-learning <softhlonlearning@gmail.com>
2025-02-16 01:57:42 +0100

    frontend: Set correct cookieDomain env for production

--- frontend/src/environment/environment.prod.ts
+++ frontend/src/environment/environment.prod.ts
@@ -11,5 +11,5 @@ export const environment = {
     production: true,
     googleClientId: '261285487242-kd1e2lhqdpfrph48o00ga0h21qrebh41.apps.googleusercontent.com',
     loginUri: 'https://java-fullstack.tech/api/v1/account/auth/google-sign-in',
-    cookieDomain: 'localhost'
+    cookieDomain: 'java-fullstack.tech',
 }
