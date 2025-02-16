731a187e83c474e0381e6e3bf7dcc041349f7379
softhlon-learning <softhlonlearning@gmail.com>
2025-02-16 20:15:55 +0100

    accounts: Add AddContactMessageService

---
+++ backend/accounts/src/main/java/tech/javafullstack/accounts/domain/AddContactMessageService.java
@@ -0,0 +1,43 @@
+// ---------------------------------------------------------------------------------------------------------------------
+// Copyright (C) JAVA-FULLSTACK.TECH - All Rights Reserved
+// Unauthorized copying of this file via any medium is strictly prohibited.
+// ---------------------------------------------------------------------------------------------------------------------
+
+package tech.javafullstack.accounts.domain;
+
+import tech.javafullstack.common.hexagonal.InboundPort;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Implementation
+// ---------------------------------------------------------------------------------------------------------------------
+
+/**
+ * Add contact message service interface.
+ */
+@InboundPort
+@FunctionalInterface
+public interface AddContactMessageService {
+
+    /**
+     * Add contact message.
+     * @param contacMessage Contact message
+     * @return Result
+     */
+    Result execute(
+          Request contacMessage);
+
+    /**
+     * Add contact message result.
+     */
+    sealed interface Result {
+        record Succeeded() implements Result {}
+        record Failed(Throwable cause) implements Result {}
+    }
+
+    record Request(
+          String subject,
+          String email,
+          String message) {
+    }
+
+}
