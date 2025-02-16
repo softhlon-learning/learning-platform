267bcb9a7fdf50732c5c64df0d228600902ee975
softhlon-learning <softhlonlearning@gmail.com>
2025-02-16 19:42:53 +0100

    accounts: Add ContactMessagesJpaRepository and entity

---
+++ backend/accounts/src/main/java/tech/javafullstack/accounts/infrastructure/ContactMessageEntity.java
@@ -0,0 +1,36 @@
+// ---------------------------------------------------------------------------------------------------------------------
+// Copyright (C) JAVA-FULLSTACK.TECH - All Rights Reserved
+// Unauthorized copying of this file via any medium is strictly prohibited.
+// ---------------------------------------------------------------------------------------------------------------------
+
+package tech.javafullstack.accounts.infrastructure;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Implementation
+// ---------------------------------------------------------------------------------------------------------------------
+
+/**
+ * Contact message JPA entity.
+ */
+@Entity
+@Getter
+@Setter
+@Builder
+@Table(name = "contact_messages", schema = "_accounts")
+@AllArgsConstructor
+@NoArgsConstructor
+class ContactMessageEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String subject;
+    private String email;
+    private String message;
+
+}
---
+++ backend/accounts/src/main/java/tech/javafullstack/accounts/infrastructure/ContactMessagesJpaRepository.java
@@ -0,0 +1,30 @@
+// ---------------------------------------------------------------------------------------------------------------------
+// Copyright (C) JAVA-FULLSTACK.TECH - All Rights Reserved
+// Unauthorized copying of this file via any medium is strictly prohibited.
+// ---------------------------------------------------------------------------------------------------------------------
+
+package tech.javafullstack.accounts.infrastructure;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Implementation
+// ---------------------------------------------------------------------------------------------------------------------
+
+/**
+ * Contact messages JPA repository interface.
+ */
+@Repository
+interface ContactMessagesJpaRepository extends CrudRepository<ContactMessageEntity, UUID> {
+
+    Optional<AccountEntity> findByEmail(
+          String email);
+
+    boolean existsByEmail(
+          String email);
+
+}
